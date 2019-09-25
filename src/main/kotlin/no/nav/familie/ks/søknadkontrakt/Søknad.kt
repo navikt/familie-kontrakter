d8234bfc0437c3804e8d6255c3d37923ce263d99
tnarland <47184872+tnarland@users.noreply.github.com>
2019-09-25 16:24:02 +0200

    Utforme modell for kontantstøttesøknaden og JSON-schema for validering

--- src/main/kotlin/no/nav/familie/ks/søknadkontrakt/Søknad.kt
+++ src/main/kotlin/no/nav/familie/ks/søknadkontrakt/Søknad.kt
@@ -1,9 +1,13 @@
 package no.nav.familie.ks.søknadkontrakt
 
 import java.time.LocalDateTime
+import javax.validation.constraints.Pattern
 
 data class Søknad(
-    val innsendtTidspunkt: LocalDateTime,
-    val aktørerArbeidYtelseIUtlandet: Set<AktørArbeidYtelseUtland>,
-    val oppgittErklæring: OppgittErklæring
+        @Pattern(regexp = "[0-9]{11}")
+        val søkerFnr: String,
+        val innsendtTidspunkt: LocalDateTime,
+        val oppgittFamilieforhold: OppgittFamilieforhold,
+        val oppgittUtlandsTilknytning: OppgittUtlandsTilknytning,
+        val oppgittErklæring: OppgittErklæring
 )

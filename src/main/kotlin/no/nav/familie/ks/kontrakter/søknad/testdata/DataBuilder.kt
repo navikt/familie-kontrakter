5aca4765bf8ebf0261cf2bbd5f63d8782e1bf4ee
Henning Håkonsen <henninghaakonsen@me.com>
2019-10-04 08:39:43 +0200

    Legger til testpersoner.

--- src/main/kotlin/no/nav/familie/ks/kontrakter/søknad/testdata/DataBuilder.kt
+++ src/main/kotlin/no/nav/familie/ks/kontrakter/søknad/testdata/DataBuilder.kt
@@ -11,6 +11,38 @@ class SøknadTestdata {
         val mapper: ObjectMapper = ObjectMapper()
 
         @JvmStatic
+        val morAktørId = "1300000000001"
+
+        @JvmStatic
+        val morPersonident = "00000000001"
+
+        @JvmStatic
+        val farAktørId = "1300000000002"
+
+        @JvmStatic
+        val farPersonident = "00000000002"
+
+        @JvmStatic
+        val barnAktørId = "1300000000003"
+
+        @JvmStatic
+        val barnPersonident = "00000000003"
+
+        @JvmStatic
+        val utenlandskFarAktørId = "1300000000004"
+
+        @JvmStatic
+        val utenlandskFarPersonident = "00000000004"
+
+        @JvmStatic
+        val utenlandskMorAktørId = "1300000000005"
+
+        @JvmStatic
+        val utenlandskMorPersonident = "00000000005"
+
+
+
+        @JvmStatic
         fun norskFamilieUtenAnnenPartOgUtenBarnehageplass(): Søknad {
             try {
                 return mapper.readValue(File(getFile("SoknadUtenBarnehageplassUtenAnnenPart.json")), Søknad::class.java)
@@ -48,7 +80,7 @@ class SøknadTestdata {
         }
 
         @JvmStatic
-        private fun tilknytningUtlandUtenBarnehageplass(): Søknad {
+        fun tilknytningUtlandUtenBarnehageplass(): Søknad {
             try {
                 return mapper.readValue(File(getFile("soknadTilknytningUtlandUtenBarnehageplass.json")), Søknad::class.java)
             } catch (e: IOException) {
@@ -56,7 +88,7 @@ class SøknadTestdata {
             }
         }
 
-        private fun getFile(filnavn: String): String {
+        fun getFile(filnavn: String): String {
             return this::class.java.getResource(String.format("json/%s", filnavn)).file
         }
     }

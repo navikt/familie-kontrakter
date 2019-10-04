6a331037b577874e88e5b9eee4c68a3640aa5e8a
Henning Håkonsen <henninghaakonsen@me.com>
2019-10-04 08:23:04 +0200

    Mindre forbedringer rundt testdata

--- src/main/kotlin/no/nav/familie/ks/kontrakter/søknad/testdata/DataBuilder.kt
+++ src/main/kotlin/no/nav/familie/ks/kontrakter/søknad/testdata/DataBuilder.kt
@@ -6,55 +6,54 @@ import java.io.File
 import java.io.IOError
 import java.io.IOException
 
-object DataBuilder {
-    val mapper: ObjectMapper = ObjectMapper()
+class DataBuilder {
+    companion object {
+        val mapper: ObjectMapper = ObjectMapper()
 
-    fun norskFamilieUtenAnnenPartOgUtenBarnehageplass(): Søknad {
-        try {
-            return mapper.readValue(File(getFile("SoknadUtenBarnehageplassUtenAnnenPart.json")), Søknad::class.java)
-        } catch (e: IOException) {
-            throw IOError(e)
+        fun norskFamilieUtenAnnenPartOgUtenBarnehageplass(): Søknad {
+            try {
+                return mapper.readValue(File(getFile("SoknadUtenBarnehageplassUtenAnnenPart.json")), Søknad::class.java)
+            } catch (e: IOException) {
+                throw IOError(e)
+            }
+
         }
 
-    }
-
-    fun norskFamilieUtenBarnehageplass(): Søknad {
-        try {
-            return mapper.readValue(File(getFile("SoknadNorskFamilieUtenBarnehageplass.json")), Søknad::class.java)
-        } catch (e: IOException) {
-            throw IOError(e)
+        fun norskFamilieUtenBarnehageplass(): Søknad {
+            try {
+                return mapper.readValue(File(getFile("SoknadNorskFamilieUtenBarnehageplass.json")), Søknad::class.java)
+            } catch (e: IOException) {
+                throw IOError(e)
+            }
         }
 
-    }
-
-    fun norskFamilieGradertBarnehageplass(): Søknad {
-        try {
-            return mapper.readValue(File(getFile("SoknadNorskFamilieGradertBarnehageplass.json")), Søknad::class.java)
-        } catch (e: IOException) {
-            throw IOError(e)
+        fun norskFamilieGradertBarnehageplass(): Søknad {
+            try {
+                return mapper.readValue(File(getFile("SoknadNorskFamilieGradertBarnehageplass.json")), Søknad::class.java)
+            } catch (e: IOException) {
+                throw IOError(e)
+            }
         }
 
-    }
-
-    fun norskFamilieMedBarnehageplass(): Søknad {
-        try {
-            return mapper.readValue(File(getFile("SoknadNorskFamilieMedBarnehageplass.json")), Søknad::class.java)
-        } catch (e: IOException) {
-            throw IOError(e)
+        fun norskFamilieMedBarnehageplass(): Søknad {
+            try {
+                return mapper.readValue(File(getFile("SoknadNorskFamilieMedBarnehageplass.json")), Søknad::class.java)
+            } catch (e: IOException) {
+                throw IOError(e)
+            }
         }
 
-    }
-
-    private fun tilknytningUtlandUtenBarnehageplass(): Søknad {
-        try {
-            return mapper.readValue(File(getFile("soknadTilknytningUtlandUtenBarnehageplass.json")), Søknad::class.java)
-        } catch (e: IOException) {
-            throw IOError(e)
+        private fun tilknytningUtlandUtenBarnehageplass(): Søknad {
+            try {
+                return mapper.readValue(File(getFile("soknadTilknytningUtlandUtenBarnehageplass.json")), Søknad::class.java)
+            } catch (e: IOException) {
+                throw IOError(e)
+            }
         }
 
+        private fun getFile(filnavn: String): String {
+            return this::class.java.getResource(String.format("json/%s", filnavn)).file
+        }
     }
 
-    private fun getFile(filnavn: String): String {
-        return this::class.java.getResource(String.format("json/%s", filnavn)).file
-    }
 }

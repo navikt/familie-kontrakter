cc6cac3ddfce0b5ff0089ad401fb663b41b5e785
Stig Helge Strøm <stig.helge.strom@nav.no>
2019-11-13 12:57:36 +0100

    toJson og convert lagt til Ressurs

--- src/main/kotlin/no/nav/familie/ks/kontrakter/Kontrakt.kt
+++ src/main/kotlin/no/nav/familie/ks/kontrakter/Kontrakt.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect
 import com.fasterxml.jackson.annotation.PropertyAccessor
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 
@@ -18,3 +19,5 @@ val objectMapper: ObjectMapper
         .registerKotlinModule()
         .registerModule(JavaTimeModule())
         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
+        .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)

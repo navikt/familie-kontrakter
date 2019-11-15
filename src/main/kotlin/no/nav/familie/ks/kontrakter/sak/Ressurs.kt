fd3b124b91cd2309fae897290c6415f0f26fea34
Stig Helge Strøm <stig.helge.strom@nav.no>
2019-11-15 08:34:45 +0100

    renamet errormelding til stacktrace

--- src/main/kotlin/no/nav/familie/ks/kontrakter/sak/Ressurs.kt
+++ src/main/kotlin/no/nav/familie/ks/kontrakter/sak/Ressurs.kt
@@ -1,22 +1,16 @@
 package no.nav.familie.ks.kontrakter.sak
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect
-import com.fasterxml.jackson.annotation.PropertyAccessor
-import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import no.nav.familie.ks.kontrakter.Kontrakt
-import no.nav.familie.ks.kontrakter.dokarkiv.api.ArkiverDokumentRequest
 import no.nav.familie.ks.kontrakter.objectMapper
+import java.io.PrintWriter
+import java.io.StringWriter
 
 data class Ressurs(
     val data: JsonNode?,
     val status: Status,
     val melding: String,
-    val errorMelding: String?
+    val stacktrace: String?
 
 ) : Kontrakt {
     enum class Status { SUKSESS, FEILET, IKKE_HENTET, IKKE_TILGANG }
@@ -28,7 +22,7 @@ data class Ressurs(
                 data = objectMapper.valueToTree(data),
                 status = Status.SUKSESS,
                 melding = "Innhenting av data var vellykket",
-                errorMelding = null
+                stacktrace = null
             )
         }
 
@@ -36,22 +30,28 @@ data class Ressurs(
             data = objectMapper.valueToTree(data),
             status = Status.SUKSESS,
             melding = melding ?: "Innhenting av data var vellykket",
-            errorMelding = null
+            stacktrace = null
         )
 
         fun failure(errorMessage: String? = null, error: Throwable? = null): Ressurs = Ressurs(
             data = null,
             status = Status.FEILET,
             melding = errorMessage ?: "Kunne ikke hente data: ${error?.message}",
-            errorMelding = error?.message ?: errorMessage
+            stacktrace = error?.textValue() ?: null
         )
 
         fun ikkeTilgang(melding: String): Ressurs = Ressurs(
             data = null,
             status = Status.IKKE_TILGANG,
             melding = melding,
-            errorMelding = ""
+            stacktrace = null
         )
+
+        private fun Throwable.textValue(): String {
+            val sw = StringWriter()
+            this.printStackTrace(PrintWriter(sw))
+            return sw.toString()
+        }
     }
 
     fun <T> convert(responseType: Class<T>): T? = objectMapper.convertValue(data, responseType)
--- src/test/kotlin/no/nav/familie/ks/kontrakter/sak/RessursTest.java
+++ src/test/kotlin/no/nav/familie/ks/kontrakter/sak/RessursTest.java
@@ -1,29 +1,38 @@
 package no.nav.familie.ks.kontrakter.sak;
 
-import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+
+import static org.junit.Assert.assertEquals;
 
 public class RessursTest {
-    private LocalDate date = LocalDate.of(2019,11,30);
+    private LocalDate date = LocalDate.of(2019, 11, 30);
+
     @Test
     public void skal_lagre_object_på_ressurs_og_hente_opp_igjen() {
 
         Ressurs ressurs = Ressurs.Companion.success(new TestObject("tekst", 42L, date), "OK");
 
         TestObject result = ressurs.convert(TestObject.class);
-        Assert.assertEquals(result.nummer, 42);
-        Assert.assertEquals(result.tekst, "tekst");
-        Assert.assertEquals(result.date, date);
+        assertEquals(result.nummer, 42);
+        assertEquals(result.tekst, "tekst");
+        assertEquals(result.date, date);
     }
 
     @Test
-    public void skal_konvertere_til_json_string() {
+    public void skal_konvertere_success_til_json_string() {
         Ressurs ressurs = Ressurs.Companion.success(new TestObject("tekst", 42L, date), "OK");
-        Assert.assertEquals(ressurs.toJson(), "{\"data\":{\"tekst\":\"tekst\",\"nummer\":42,\"date\":\"2019-11-30\"},\"status\":\"SUKSESS\",\"melding\":\"OK\",\"errorMelding\":null}");
+        assertEquals(ressurs.toJson(), "{\"data\":{\"tekst\":\"tekst\",\"nummer\":42,\"date\":\"2019-11-30\"},\"status\":\"SUKSESS\",\"melding\":\"OK\",\"stacktrace\":null}");
+    }
 
+    @Test
+    public void skal_konvertere_stacktrace_to_data() {
+        Ressurs ressurs = Ressurs.Companion.failure("Dette er errormeldingen", new RuntimeException("Dette er feilkoden"));
+
+        Assertions.assertTrue(ressurs.getStacktrace().contains("java.lang.RuntimeException: Dette er feilkoden\n" +
+                "\tat no.nav.familie.ks.kontrakter.sak.RessursTest.skal_konvertere_stacktrace_to_data"));
     }
 
     static class TestObject {

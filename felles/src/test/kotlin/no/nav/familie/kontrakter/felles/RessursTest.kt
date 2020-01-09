891d75651675d007e74be2cc20a67ba6738c6817
Bent J. Lorentzen <bent.lorentzen@nav.no>
2020-01-09 13:31:59 +0100

    Fikset plattformavhengig test og konvertert til kotlin

--- felles/src/test/kotlin/no/nav/familie/kontrakter/felles/RessursTest.kt
+++ felles/src/test/kotlin/no/nav/familie/kontrakter/felles/RessursTest.kt
@@ -1,52 +1,48 @@
-package no.nav.familie.kontrakter.felles;
+package no.nav.familie.kontrakter.felles
 
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
+import no.nav.familie.kontrakter.felles.Ressurs.Companion.failure
+import no.nav.familie.kontrakter.felles.Ressurs.Companion.success
+import org.junit.Assert
+import org.junit.Test
+import org.junit.jupiter.api.Assertions
+import java.time.LocalDate
 
-import java.time.LocalDate;
-
-import static org.junit.Assert.assertEquals;
-
-public class RessursTest {
-    private LocalDate date = LocalDate.of(2019, 11, 30);
-
+class RessursTest {
+    private val date = LocalDate.of(2019, 11, 30)
     @Test
-    public void skal_lagre_object_på_ressurs_og_hente_opp_igjen() {
-
-        Ressurs<TestObject> ressurs = Ressurs.Companion.success(new TestObject("tekst", 42L, date), "OK");
-
-        TestObject result = ressurs.getData();
-        assertEquals(result.nummer, 42);
-        assertEquals(result.tekst, "tekst");
-        assertEquals(result.date, date);
+    fun `skal lagre object på ressurs og hente opp igjen`() {
+        val (result) = success(TestObject("tekst", 42L, date), "OK")
+        Assert.assertEquals(result?.nummer, 42L)
+        Assert.assertEquals(result?.tekst, "tekst")
+        Assert.assertEquals(result?.date, date)
     }
 
     @Test
-    public void skal_konvertere_success_til_json_string() {
-        Ressurs ressurs = Ressurs.Companion.success(new TestObject("tekst", 42L, date), "OK");
-        assertEquals(ressurs.toJson(), "{\"data\":{\"tekst\":\"tekst\",\"nummer\":42,\"date\":\"2019-11-30\"},\"status\":\"SUKSESS\",\"melding\":\"OK\",\"stacktrace\":null}");
+    fun `skal konvertere success til json string`() {
+        val ressurs: Ressurs<*> = success(TestObject("tekst", 42L, date), "OK")
+        Assert.assertEquals(
+            ressurs.toJson(),
+            "{\"data\":{\"tekst\":\"tekst\",\"nummer\":42,\"date\":\"2019-11-30\"}," +
+                    "\"status\":\"SUKSESS\",\"melding\":\"OK\",\"stacktrace\":null}"
+        )
     }
 
     @Test
-    public void skal_konvertere_stacktrace_to_data() {
-        Ressurs ressurs = Ressurs.Companion.failure("Dette er errormeldingen", new RuntimeException("Dette er feilkoden"));
-
-        Assertions.assertTrue(ressurs.getStacktrace().contains("java.lang.RuntimeException: Dette er feilkoden\n" +
-                "\tat no.nav.familie.kontrakter.felles.RessursTest.skal_konvertere_stacktrace_to_data"));
+    fun `skal konvertere stacktrace to data`() {
+        val (_, _, _, stacktrace) =
+            failure<Any>("Dette er errormeldingen", RuntimeException("Dette er feilkoden"))
+        Assertions.assertTrue(
+            stacktrace!!.contains(
+                "java.lang.RuntimeException: Dette er feilkoden" +
+                        System.getProperty("line.separator") +
+                        "\tat no.nav.familie.kontrakter.felles.RessursTest.skal konvertere stacktrace to data"
+            )
+        )
     }
 
-    static class TestObject {
-        private String tekst;
-        private long nummer;
-        private LocalDate date;
-
-        public TestObject() {
-        }
-
-        public TestObject(String tekst, long nummer, LocalDate date) {
-            this.tekst = tekst;
-            this.nummer = nummer;
-            this.date = date;
-        }
-    }
+    internal class TestObject(
+        val tekst: String,
+        val nummer: Long,
+        val date: LocalDate?
+    )
 }

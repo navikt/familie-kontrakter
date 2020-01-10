c274bc165cc9e604306cee87100f044e45457639
Bent Lorentzen <bent.lorentzen@nav.no>
2020-01-10 13:33:17 +0100

    Lagt til isForsøkFerdigstill for bakoverkompatibilitet.

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/arkivering/ArkiverDokumentRequest.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/arkivering/ArkiverDokumentRequest.kt
@@ -3,6 +3,9 @@ package no.nav.familie.kontrakter.felles.arkivering
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotEmpty
 
-data class ArkiverDokumentRequest(@field:NotBlank val fnr: String,
-                                  @field:NotBlank val forsøkFerdigstill: Boolean,
-                                  @field:NotEmpty val dokumenter: List<Dokument>)
+class ArkiverDokumentRequest(@field:NotBlank val fnr: String,
+                             @field:NotBlank val forsøkFerdigstill: Boolean,
+                             @field:NotEmpty val dokumenter: List<Dokument>) {
+
+    val isForsøkFerdigstill = forsøkFerdigstill
+}

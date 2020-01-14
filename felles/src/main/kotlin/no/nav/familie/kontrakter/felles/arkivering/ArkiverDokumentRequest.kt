6b9203cd786def1a47d40c6cbd95b1414211935c
Bent Lorentzen <bent.lorentzen@nav.no>
2020-01-14 11:22:25 +0100

    bugfix arkivering.

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/arkivering/ArkiverDokumentRequest.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/arkivering/ArkiverDokumentRequest.kt
@@ -2,9 +2,10 @@ package no.nav.familie.kontrakter.felles.arkivering
 
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
 
 class ArkiverDokumentRequest(@field:NotBlank val fnr: String,
-                             @field:NotBlank val forsøkFerdigstill: Boolean,
+                             @field:NotNull val forsøkFerdigstill: Boolean,
                              @field:NotEmpty val dokumenter: List<Dokument>) {
 
     val isForsøkFerdigstill = forsøkFerdigstill
--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/arkivering/ArkiverDokumentResponse.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/arkivering/ArkiverDokumentResponse.kt
@@ -1,6 +1,7 @@
 package no.nav.familie.kontrakter.felles.arkivering
 
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 
 class ArkiverDokumentResponse(@field:NotBlank val journalpostId: String,
-                              @field:NotBlank val ferdigstilt: Boolean)
+                              @field:NotNull val ferdigstilt: Boolean)

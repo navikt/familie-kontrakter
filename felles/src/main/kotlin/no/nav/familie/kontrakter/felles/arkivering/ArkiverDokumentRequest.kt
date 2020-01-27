584f500efcf722332246700335172ba8ff0aa2d5
tnarland <47184872+tnarland@users.noreply.github.com>
2020-01-27 16:10:46 +0100

    Update ArkiverDokumentRequest.kt (#46)

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/arkivering/ArkiverDokumentRequest.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/arkivering/ArkiverDokumentRequest.kt
@@ -6,7 +6,9 @@ import javax.validation.constraints.NotNull
 
 data class ArkiverDokumentRequest(@field:NotBlank val fnr: String,
                                   @field:NotNull val forsøkFerdigstill: Boolean,
-                                  @field:NotEmpty val dokumenter: List<Dokument>) {
-
+                                  @field:NotEmpty val dokumenter: List<Dokument>,
+                                  val fagsakId: String?= null,
+                                  val journalførendeEnhet: String? = null) { //Skal ikke settes for innkommende hvis Ruting gjøres av BRUT001
+  
     val isForsøkFerdigstill = forsøkFerdigstill
 }

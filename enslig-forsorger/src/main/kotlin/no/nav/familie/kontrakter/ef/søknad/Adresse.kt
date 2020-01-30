6c9a7cdcda9ef73abf53212bdf20626a2343b3ce
Nina Kjekstad <ninakjekstad@Ninas-MacBook-Pro.local>
2020-01-30 14:41:23 +0100

    Forenkle adresse

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Adresse.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Adresse.kt
@@ -1,9 +1,6 @@
 package no.nav.familie.kontrakter.ef.søknad
 
-data class Adresse(val gatenavn: String? = null,
-                   val husnummer: Int? = null,
-                   val husbokstav: String? = null,
-                   val bolignummer: String? = null,
+data class Adresse(val adresse: String? = null,
                    val postnummer: String,
                    val poststedsnavn: String? = null,
                    val land: String? = null)

fde9dcaee99adc43565e3550b29b816825144e47
Stig Strøm <stig.helge.strom@nav.no>
2020-01-31 15:23:03 +0100

    Flyttet Oppgave til felles (#50)

---
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/oppgave/Oppgave.kt
@@ -0,0 +1,11 @@
+package no.nav.familie.kontrakter.felles.oppgave
+
+import javax.validation.constraints.Pattern
+
+data class Oppgave(
+    @field:Pattern(regexp = "[0-9]{13}")
+    val aktorId: String,
+    val journalpostId: String,
+    val eksisterendeOppgaveId: String?,
+    val beskrivelse: String
+)

b142f345fd89188d3e5838818edbc9a82e1baadc
bent-lorentzen <54444412+bent-lorentzen@users.noreply.github.com>
2020-03-31 15:07:36 +0200

    Lagt inn medlemskapsinfo fra integrasjon. (#68)

---
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/medlemskap/PeriodeInfo.kt
@@ -0,0 +1,11 @@
+package no.nav.familie.kontrakter.felles.medlemskap
+
+import java.time.LocalDate
+
+data class PeriodeInfo(val periodeStatus: PeriodeStatus,
+                      val periodeStatusÅrsak: PeriodeStatusÅrsak?,
+                      val fom: LocalDate,
+                      val tom: LocalDate,
+                      val gjelderMedlemskapIFolketrygden: Boolean,
+                      val grunnlag: String,
+                      val dekning: String? = null)

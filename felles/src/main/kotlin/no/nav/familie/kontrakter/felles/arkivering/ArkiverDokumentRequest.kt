0f21655f4e251be61d8ddbf2087d925779be8f11
Bent J. Lorentzen <bent.lorentzen@nav.no>
2020-05-04 15:53:29 +0200

    Skilt mellom hoveddokumenter og vedlegsdokumenter.

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/arkivering/ArkiverDokumentRequest.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/arkivering/ArkiverDokumentRequest.kt
@@ -5,7 +5,9 @@ import javax.validation.constraints.NotEmpty
 
 data class ArkiverDokumentRequest(@field:NotBlank val fnr: String,
                                   val forsøkFerdigstill: Boolean,
-                                  @field:NotEmpty val dokumenter: List<Dokument>,
+                                  @field:NotEmpty val hoveddokumentvarianter: List<Dokument>,
+                                  val vedleggsdokumenter: List<Dokument> = emptyList(),
                                   val fagsakId: String? = null,
-                                  val journalførendeEnhet: String? = null) //Skal ikke settes for innkommende hvis Ruting gjøres av BRUT001
+        //Skal ikke settes for innkommende hvis Ruting gjøres av BRUT001
+                                  val journalførendeEnhet: String? = null)
 

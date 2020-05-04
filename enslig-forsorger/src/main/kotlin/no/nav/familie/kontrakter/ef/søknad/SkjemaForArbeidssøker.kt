70e49752271d29ab976dc9fbe417dd1c1aab5ae7
ma10s <mattis.janitz@nav.no>
2020-05-04 14:38:24 +0200

    Wrap fødselsnummer og navn i søknadsfelt - personaliaklasse

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/SkjemaForArbeidssøker.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/SkjemaForArbeidssøker.kt
@@ -1,6 +1,8 @@
 package no.nav.familie.kontrakter.ef.søknad
 
 
-data class SkjemaForArbeidssøker(val fødselsnummer: Søknadsfelt<Fødselsnummer>,
-                                 val navn: Søknadsfelt<String>,
+data class SkjemaForArbeidssøker(val personalia : Søknadsfelt<SkjemaPersonalia>,
                                  val arbeidssøker: Søknadsfelt<Arbeidssøker>)
+
+data class SkjemaPersonalia ( val fødselsnummer: Søknadsfelt<Fødselsnummer>,
+                              val navn: Søknadsfelt<String>)

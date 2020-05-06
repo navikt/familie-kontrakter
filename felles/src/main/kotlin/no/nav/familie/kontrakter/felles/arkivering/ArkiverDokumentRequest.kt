168c35717b6978d19dc1f0ef7756c79a9405a7af
Stig Strøm <stig.helge.strom@nav.no>
2020-05-06 09:54:27 +0200

    Revert "Skilt mellom hoveddokumenter og vedlegsdokumenter. (#86)" (#90)
    This reverts commit 3ca9e4b97952dcc92c778f4b86d9841661fd92c0.

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/arkivering/ArkiverDokumentRequest.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/arkivering/ArkiverDokumentRequest.kt
@@ -5,9 +5,7 @@ import javax.validation.constraints.NotEmpty
 
 data class ArkiverDokumentRequest(@field:NotBlank val fnr: String,
                                   val forsøkFerdigstill: Boolean,
-                                  @field:NotEmpty val hoveddokumentvarianter: List<Dokument>,
-                                  val vedleggsdokumenter: List<Dokument> = emptyList(),
+                                  @field:NotEmpty val dokumenter: List<Dokument>,
                                   val fagsakId: String? = null,
-        //Skal ikke settes for innkommende hvis Ruting gjøres av BRUT001
-                                  val journalførendeEnhet: String? = null)
+                                  val journalførendeEnhet: String? = null) //Skal ikke settes for innkommende hvis Ruting gjøres av BRUT001
 

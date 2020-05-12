79620a00633eba31df5f5e4f07b2d1e91bcc0339
ma10s <mattis.janitz@nav.no>
2020-05-12 14:13:07 +0200

    Vil legge til land for annen forelder. (Dersom ikke bosatt i norge vil vi vite hvor) Vil gjøre kanIkkeOppgiAnnenForelderFar og bosattNorge påkrevd

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/AnnenForelder.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/AnnenForelder.kt
@@ -1,8 +1,9 @@
 package no.nav.familie.kontrakter.ef.søknad
 
-data class AnnenForelder(val kanIkkeOppgiAnnenForelderFar: Søknadsfelt<Boolean>? = null,
+data class AnnenForelder(val kanIkkeOppgiAnnenForelderFar: Søknadsfelt<Boolean>,
                          val ikkeOppgittAnnenForelderBegrunnelse: Søknadsfelt<String>? = null,
-                         val bosattNorge: Søknadsfelt<Boolean>? = null,
+                         val bosattNorge: Søknadsfelt<Boolean>,
+                         val land: Søknadsfelt<String>? = null,
                          val person: Søknadsfelt<PersonMinimum>? = null,
                          val adresse: Søknadsfelt<Adresse>? = null)
 

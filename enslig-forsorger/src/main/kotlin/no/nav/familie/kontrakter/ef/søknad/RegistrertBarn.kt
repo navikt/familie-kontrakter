51e8b6fbf3fb2b16954147beb203feb71467fa4a
ma10s <mattis.janitz@nav.no>
2020-05-25 08:22:32 +0200

    Tester: nye felt - samvær: borAnnenForelderISammeHusBeskrivelse og registrert barn: ikkeRegistrertPåSøkersAdresseBeskrivelse

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/RegistrertBarn.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/RegistrertBarn.kt
@@ -3,6 +3,6 @@ package no.nav.familie.kontrakter.ef.søknad
 data class RegistrertBarn(val navn: Søknadsfelt<String>,
                           val fødselsnummer: Søknadsfelt<Fødselsnummer>,
                           val harSammeAdresse: Søknadsfelt<Boolean>,
-                          val ikkeRegistrertPåSøkersAdresseBeskrivelse: Søknadsfelt<String>,
+                          val ikkeRegistrertPåSøkersAdresseBeskrivelse: Søknadsfelt<String>?,
                           val annenForelder: Søknadsfelt<AnnenForelder>? = null,
                           val samvær: Søknadsfelt<Samvær>? = null)

e1145cf203e062ee4b840a1ad732a738fac7a409
ma10s <mattis.janitz@nav.no>
2020-06-05 17:08:13 +0200

    Dersom søker f.eks er i fast stilling spør vi ikke om bruker har en sluttdato. Setter ? på denne.

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Arbeidsgiver.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Arbeidsgiver.kt
@@ -6,5 +6,5 @@ import java.time.LocalDate
 data class Arbeidsgiver(val arbeidsgivernavn: Søknadsfelt<String>,
                         val stillingsprosent: Søknadsfelt<Int>,
                         val fastEllerMidlertidig: Søknadsfelt<String>,
-                        val harDuEnSluttdato: Søknadsfelt<Boolean>,
+                        val harDuEnSluttdato: Søknadsfelt<Boolean>?,
                         val sluttdato: Søknadsfelt<LocalDate>? = null)

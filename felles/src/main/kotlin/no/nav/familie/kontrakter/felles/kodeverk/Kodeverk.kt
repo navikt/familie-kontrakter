e72105a196481d1ab49055c2158eee1a681526e7
Johan Blomgren <blommish@users.noreply.github.com>
2020-06-10 08:23:50 +0200

    Flyttet Dtooer for kodeverk som eksponeres i integrasjoner til kontra… (#110)
    * Flyttet Dtooer for kodeverk som eksponeres i integrasjoner til kontrakter

---
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/kodeverk/Kodeverk.kt
@@ -0,0 +1,26 @@
+package no.nav.familie.kontrakter.felles.kodeverk
+
+import java.time.LocalDate
+
+data class KodeverkDto(val betydninger: Map<String, List<BetydningDto>>)
+
+data class BetydningDto(val gyldigFra: LocalDate,
+                        val gyldigTil: LocalDate,
+                        val beskrivelser: Map<String, BeskrivelseDto>)
+
+data class BeskrivelseDto(val term: String,
+                          val tekst: String)
+
+enum class KodeverkSpråk(val kode: String) {
+    BOKMÅL("nb")
+}
+
+private fun LocalDate.mellom(fra: LocalDate, til: LocalDate) =
+        this.isEqual(fra) || this.isEqual(til) || (this.isAfter(fra) && this.isBefore(til))
+
+fun KodeverkDto.hentGjelende(kode: String,
+                             gjeldendeDato: LocalDate = LocalDate.now(),
+                             språk: KodeverkSpråk = KodeverkSpråk.BOKMÅL) =
+        betydninger[kode]
+                ?.firstOrNull { gjeldendeDato.mellom(it.gyldigFra, it.gyldigTil) }
+                ?.beskrivelser?.get(språk.kode)?.term
---
+++ felles/src/test/kotlin/no/nav/familie/kontrakter/felles/kodeverk/KodeverkTest.kt
@@ -0,0 +1,37 @@
+package no.nav.familie.kontrakter.felles.kodeverk
+
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import kotlin.test.assertEquals
+
+internal class KodeverkTest {
+
+    @Test
+    internal fun `finner gjeldende term når det kun finnes en betydning`() {
+        val kodeverk = KodeverkDto(betydninger = mapOf("NOR" to listOf(
+                BetydningDto(gyldigFra = LocalDate.of(2020, 1, 15),
+                             gyldigTil = LocalDate.of(2020, 1, 16),
+                             beskrivelser = mapOf("nb" to BeskrivelseDto("Norge", "NorgeTekst"))))))
+        assertEquals("Norge", kodeverk.hentGjelende("NOR", LocalDate.of(2020, 1, 16)))
+        assertNull(kodeverk.hentGjelende("NOR"))
+    }
+
+    @Test
+    internal fun `finner gjelde term når det finnes flere betyninger`() {
+        val kodeverk = KodeverkDto(betydninger = mapOf("NOR" to listOf(
+                BetydningDto(gyldigFra = LocalDate.of(2020, 1, 15),
+                             gyldigTil = LocalDate.of(2020, 1, 16),
+                             beskrivelser = mapOf("nb" to BeskrivelseDto("Sverige", "SverigeTekst"))),
+                BetydningDto(gyldigFra = LocalDate.of(2020, 1, 17),
+                             gyldigTil = LocalDate.of(2099, 1, 16),
+                             beskrivelser = mapOf("nb" to BeskrivelseDto("Norge", "NorgeTekst"))))))
+        assertEquals("Sverige", kodeverk.hentGjelende("NOR", LocalDate.of(2020, 1, 16)))
+        assertEquals("Norge", kodeverk.hentGjelende("NOR"))
+    }
+
+    @Test
+    internal fun `finner ingen gjeldende term når det ikke finnes en betydning`() {
+        assertNull(KodeverkDto(betydninger = mapOf("NOR" to emptyList())).hentGjelende("NOR"))
+    }
+}

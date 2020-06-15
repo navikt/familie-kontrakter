4acfbd51b639267d1e36cf433b839bc73d30224e
bent-lorentzen <54444412+bent-lorentzen@users.noreply.github.com>
2020-06-15 09:38:15 +0200

    Slått sammen nyttBarn og rregistrertBarn til Barn. (#120)
    * Slått sammen nyttBarn og registrertBarn til Barn.

---
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Barn.kt
@@ -0,0 +1,13 @@
+package no.nav.familie.kontrakter.ef.søknad
+
+import java.time.LocalDate
+
+data class Barn(val navn: Søknadsfelt<String>? = null,
+                val fødselsnummer: Søknadsfelt<Fødselsnummer>? = null,
+                val harSkalHaSammeAdresse: Søknadsfelt<Boolean>,
+                val ikkeRegistrertPåSøkersAdresseBeskrivelse: Søknadsfelt<String>?,
+                val erBarnetFødt: Søknadsfelt<Boolean>,
+                val fødselTermindato: Søknadsfelt<LocalDate>? = null,
+                val terminbekreftelse: Søknadsfelt<List<Dokument>>? = null,
+                val annenForelder: Søknadsfelt<AnnenForelder>? = null,
+                val samvær: Søknadsfelt<Samvær>? = null)
--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/RegistrertBarn.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.familie.kontrakter.ef.søknad
-
-data class RegistrertBarn(val navn: Søknadsfelt<String>,
-                          val fødselsnummer: Søknadsfelt<Fødselsnummer>,
-                          val harSammeAdresse: Søknadsfelt<Boolean>,
-                          val ikkeRegistrertPåSøkersAdresseBeskrivelse: Søknadsfelt<String>?,
-                          val annenForelder: Søknadsfelt<AnnenForelder>? = null,
-                          val samvær: Søknadsfelt<Samvær>? = null)

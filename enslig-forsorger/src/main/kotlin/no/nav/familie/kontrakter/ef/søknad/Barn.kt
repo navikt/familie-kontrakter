a3e56af0d796b07b9663b16baa72e49c717a3dc5
Bent J. Lorentzen <bent.lorentzen@nav.no>
2020-06-15 08:46:01 +0200

    Slått sammen nyttBarn og rregistrertBarn til Barn.

---
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Barn.kt
@@ -0,0 +1,14 @@
+package no.nav.familie.kontrakter.ef.søknad
+
+import java.time.LocalDate
+
+data class Barn(val navn: Søknadsfelt<String>? = null,
+                val fødselsnummer: Søknadsfelt<Fødselsnummer>? = null,
+                val harSkalHaSammeAdresse: Søknadsfelt<Boolean>,
+                val ikkeRegistrertPåSøkersAdresseBeskrivelse: Søknadsfelt<String>?,
+                val erBarnetFødt: Søknadsfelt<Boolean>,
+                val fødselTermindato: Søknadsfelt<LocalDate>,
+                val terminbekreftelse: Søknadsfelt<Dokument>? = null,
+                val annenForelder: Søknadsfelt<AnnenForelder>? = null,
+                val samvær: Søknadsfelt<Samvær>? = null
+)

c642ceea128f4e7640c7e0c0b35b2edc8e70ed03
charliemidtlyng <charlie.midtlyng@bekk.no>
2020-06-18 12:55:23 +0200

    Gjort sivilstandsplaner.harPlaner frivillig

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Sivilstandsplaner.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Sivilstandsplaner.kt
@@ -2,6 +2,6 @@ package no.nav.familie.kontrakter.ef.søknad
 
 import java.time.LocalDate
 
-data class Sivilstandsplaner(val harPlaner: Søknadsfelt<Boolean>,
+data class Sivilstandsplaner(val harPlaner: Søknadsfelt<Boolean>? = null,
                              val fraDato: Søknadsfelt<LocalDate>? = null,
                              val vordendeSamboerEktefelle: Søknadsfelt<PersonMinimum>? = null)

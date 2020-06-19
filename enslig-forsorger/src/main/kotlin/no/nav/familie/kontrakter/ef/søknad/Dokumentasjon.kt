1c9b52e052af807778e78e182026736e99789544
Johan Blomgren <johan.blomgren@nav.no>
2020-06-19 12:20:31 +0200

    Wrapper harSendtInnTidligere i søknadsfelt

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Dokumentasjon.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Dokumentasjon.kt
@@ -1,3 +1,3 @@
 package no.nav.familie.kontrakter.ef.søknad
 
-data class Dokumentasjon(val harSendtInnTidligere: Boolean, val dokumenter: List<Dokument>)
+data class Dokumentasjon(val harSendtInnTidligere: Søknadsfelt<Boolean>, val dokumenter: List<Dokument>)

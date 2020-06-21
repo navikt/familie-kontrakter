1a9496dfedcec6e6f530d29a577a401c761826b9
ma10s <mattis.janitz@nav.no>
2020-06-21 13:57:24 +0200

    Sett kanDuSkaffeBarnepassInnenEnUke optional.

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Arbeidssøker.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Arbeidssøker.kt
@@ -3,7 +3,7 @@ package no.nav.familie.kontrakter.ef.søknad
 data class Arbeidssøker(val registrertSomArbeidssøkerNav: Søknadsfelt<Boolean>,
                         val villigTilÅTaImotTilbudOmArbeid: Søknadsfelt<Boolean>,
                         val kanDuBegynneInnenEnUke: Søknadsfelt<Boolean>,
-                        val kanDuSkaffeBarnepassInnenEnUke: Søknadsfelt<Boolean>,
+                        val kanDuSkaffeBarnepassInnenEnUke: Søknadsfelt<Boolean>?,
                         val hvorØnskerDuArbeid: Søknadsfelt<String>,
                         val ønskerDuMinst50ProsentStilling: Søknadsfelt<Boolean>,
                         val ikkeVilligTilÅTaImotTilbudOmArbeidDokumentasjon: Søknadsfelt<Dokumentasjon>? = null)

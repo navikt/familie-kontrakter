03de18abf7c0ca62d9a6daf3bca974564fffa154
ma10s <mattis.janitz@nav.no>
2020-06-30 13:34:06 +0200

    Sett kanIkkeOppgiAnnenForelderFar optional.

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/AnnenForelder.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/AnnenForelder.kt
@@ -1,6 +1,6 @@
 package no.nav.familie.kontrakter.ef.søknad
 
-data class AnnenForelder(val kanIkkeOppgiAnnenForelderFar: Søknadsfelt<Boolean>,
+data class AnnenForelder(val kanIkkeOppgiAnnenForelderFar: Søknadsfelt<Boolean>?,
                          val ikkeOppgittAnnenForelderBegrunnelse: Søknadsfelt<String>? = null,
                          val bosattNorge: Søknadsfelt<Boolean>? = null,
                          val land: Søknadsfelt<String>? = null,

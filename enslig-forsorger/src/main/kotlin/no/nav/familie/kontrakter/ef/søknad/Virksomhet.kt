01e1ee3a99b595757c94b2caeb202beab58cd347
Johan Blomgren <blommish@users.noreply.github.com>
2020-06-30 13:42:18 +0200

    Lagt til mulighet for å sende inn dokumentasjon på virksomhet (#133)

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Virksomhet.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Virksomhet.kt
@@ -1,3 +1,4 @@
 package no.nav.familie.kontrakter.ef.søknad
 
-data class Virksomhet(val virksomhetsbeskrivelse: Søknadsfelt<String>)
+data class Virksomhet(val virksomhetsbeskrivelse: Søknadsfelt<String>,
+                      val dokumentasjon: Søknadsfelt<Dokumentasjon>? = null)

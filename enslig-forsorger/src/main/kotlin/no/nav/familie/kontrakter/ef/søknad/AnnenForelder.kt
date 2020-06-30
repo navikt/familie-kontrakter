9a99ccbf85b15b31a1670713bf865d0b25955c9f
Mattis Janitz <mattis.janitz@nav.no>
2020-06-30 13:43:23 +0200

    Kan ikke oppgi annen forelder far optional (#134)
    * Sett kanIkkeOppgiAnnenForelderFar optional.
    
    * Fjerner kanIkkeOppgiAnnenForelderFar.

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/AnnenForelder.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/AnnenForelder.kt
@@ -1,7 +1,6 @@
 package no.nav.familie.kontrakter.ef.søknad
 
-data class AnnenForelder(val kanIkkeOppgiAnnenForelderFar: Søknadsfelt<Boolean>,
-                         val ikkeOppgittAnnenForelderBegrunnelse: Søknadsfelt<String>? = null,
+data class AnnenForelder(val ikkeOppgittAnnenForelderBegrunnelse: Søknadsfelt<String>? = null,
                          val bosattNorge: Søknadsfelt<Boolean>? = null,
                          val land: Søknadsfelt<String>? = null,
                          val person: Søknadsfelt<PersonMinimum>? = null)

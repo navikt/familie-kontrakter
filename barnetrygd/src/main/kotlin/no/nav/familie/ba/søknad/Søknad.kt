ca81360300545a57f4f726d6cef4bcc1b71a1f92
Henrik Jackson <henrik.m.jackson@gmail.com>
2020-07-13 14:21:33 +0200

    Oppdater kontrakt for ba-søknad til å være lik søknadsobjektet i frontend

--- barnetrygd/src/main/kotlin/no/nav/familie/ba/søknad/Søknad.kt
+++ barnetrygd/src/main/kotlin/no/nav/familie/ba/søknad/Søknad.kt
@@ -2,12 +2,26 @@ package main.kotlin.no.nav.familie.ba.søknad
 
 import main.kotlin.no.nav.familie.ba.Søknadstype
 
+data class Søker(
+        val navn: Søknadsfelt<String>
+)
 
 data class Søknad(
-        val søknadstype: Søknadsfelt<Søknadstype>
+        val søknadstype: Søknadsfelt<Søknadstype>,
+        val søker: Søker,
+        val barn: List<Barn>
 )
 
 data class Søknadsfelt<T>(
         val label: String,
         val verdi: T
+)
+
+data class Barn(
+        val navn: Søknadsfelt<String>,
+        val alder: Søknadsfelt<Int>,
+        val fødselsdato: Søknadsfelt<String>,
+        val ident: Søknadsfelt<String>,
+        val borMedSøker: Søknadsfelt<Boolean>,
+        val medISøknad: Søknadsfelt<Boolean>
 )

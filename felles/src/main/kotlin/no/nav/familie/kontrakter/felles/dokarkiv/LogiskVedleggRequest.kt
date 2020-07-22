cbf4d36f0ae3607bfbb22feb6a36267a788b8f04
Bent J. Lorentzen <bent.lorentzen@nav.no>
2020-07-22 12:49:42 +0200

    To klasser til.

---
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/dokarkiv/LogiskVedleggRequest.kt
@@ -0,0 +1,3 @@
+package no.nav.familie.kontrakter.felles.dokarkiv
+
+data class LogiskVedleggRequest(val tittel: String)
---
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/dokarkiv/LogiskVedleggResponse.kt
@@ -0,0 +1,3 @@
+package no.nav.familie.kontrakter.felles.dokarkiv
+
+data class LogiskVedleggResponse(val logiskVedleggId: Long)

7da223aa602cb7fee93281d1241a7181fa1e4c9e
jonatan <jonaengs@gmail.com>
2020-07-23 11:39:37 +0200

    separer tittel og søknadskode

--- barnetrygd/src/main/kotlin/no/nav/familie/ba/Søknadstype.kt
+++ barnetrygd/src/main/kotlin/no/nav/familie/ba/Søknadstype.kt
@@ -1,8 +1,8 @@
 package main.kotlin.no.nav.familie.ba
 
-enum class Søknadstype(val tittel: String) {
-    IKKE_SATT("SØKNADSTYPE MANGLER"),
-    ORDINÆR("Søknad om barnetrygd ordinær - NAV 33-00.07"),
-    UTVIDET("Søknad om utvidet barnetrygd - NAV 33-00.09"),
-    EØS("Tilleggsskjema ved krav om utbetaling av barnetrygd og/eller kontantstøtte på grunnlag av regler om eksport etter EØS-avtalen - NAV 34-00.15")
+enum class Søknadstype(val tittel: String, val søknadskode: String) {
+    IKKE_SATT("SØKNADSTYPE MANGLER", "SØKNADSTYPE MANGLER"),
+    ORDINÆR("Søknad om barnetrygd ordinær", "NAV 33-00.07"),
+    UTVIDET("Søknad om utvidet barnetrygd", "NAV 33-00.09"),
+    EØS("Tilleggsskjema ved krav om utbetaling av barnetrygd og/eller kontantstøtte på grunnlag av regler om eksport etter EØS-avtalen", "NAV 34-00.15")
 }

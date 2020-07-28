61a4305fd49f54c66a9e2684f55958c67a6b4254
bent-lorentzen <54444412+bent-lorentzen@users.noreply.github.com>
2020-07-28 10:31:36 +0200

    Lagt til QueryObject og serialiseringsinfo på datoer. (#156)

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/oppgave/FinnOppgaveRequest.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/oppgave/FinnOppgaveRequest.kt
@@ -1,5 +1,7 @@
 package no.nav.familie.kontrakter.felles.oppgave
 
+import no.nav.familie.kontrakter.felles.abstraction.QueryObject
+import org.springframework.format.annotation.DateTimeFormat
 import java.time.LocalDate
 import java.time.LocalDateTime
 
@@ -11,11 +13,17 @@ data class FinnOppgaveRequest(val tema: Tema,
                               val journalpostId: String? = null,
                               val tilordnetRessurs: String? = null,
                               val tildeltRessurs: Boolean? = null,
+                              @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
                               val opprettetFomTidspunkt: LocalDateTime? = null,
+                              @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
                               val opprettetTomTidspunkt: LocalDateTime? = null,
+                              @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
                               val fristFomDato: LocalDate? = null,
+                              @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
                               val fristTomDato: LocalDate? = null,
+                              @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
                               val aktivFomDato: LocalDate? = null,
+                              @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
                               val aktivTomDato: LocalDate? = null,
                               val limit: Long? = null,
-                              val offset: Long? = null)
+                              val offset: Long? = null) : QueryObject()

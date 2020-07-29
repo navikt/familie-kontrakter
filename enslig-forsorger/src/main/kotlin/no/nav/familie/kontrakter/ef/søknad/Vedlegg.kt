0c8843e4e8fe72128ecb74e23f8d72e3bdbc6e3a
Johan Blomgren <johan.blomgren@nav.no>
2020-07-29 10:52:41 +0200

    Opprydding vedlegg, fjerning av bytes som ikke er i bruk siden vi nå bruker multipart for å sende vedlegg

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Vedlegg.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Vedlegg.kt
@@ -1,35 +1,3 @@
 package no.nav.familie.kontrakter.ef.søknad
 
-data class Vedlegg(val id: String, val navn: String, val tittel: String, val bytes: ByteArray?) {
-
-    override fun toString(): String {
-        return "Vedlegg(id='$id', navn='$navn', tittel='$tittel')"
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Vedlegg
-
-        if (id != other.id) return false
-        if (navn != other.navn) return false
-        if (tittel != other.tittel) return false
-        if (bytes != null) {
-            if (other.bytes == null) return false
-            if (!bytes.contentEquals(other.bytes)) return false
-        } else if (other.bytes != null) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = id.hashCode()
-        result = 31 * result + navn.hashCode()
-        result = 31 * result + tittel.hashCode()
-        result = 31 * result + (bytes?.contentHashCode() ?: 0)
-        return result
-    }
-
-
-}
+data class Vedlegg(val id: String, val navn: String, val tittel: String)

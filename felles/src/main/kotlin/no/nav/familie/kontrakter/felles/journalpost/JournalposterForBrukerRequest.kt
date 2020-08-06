4d87831d6a6758444b715511260a9411a88800a0
Bent J. Lorentzen <bent.lorentzen@nav.no>
2020-08-06 13:12:57 +0200

    graphql aksepterer ikke ø

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/journalpost/JournalposterForBrukerRequest.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/journalpost/JournalposterForBrukerRequest.kt
@@ -1,9 +1,6 @@
 package no.nav.familie.kontrakter.felles.journalpost
 
-import no.nav.familie.kontrakter.felles.abstraction.QueryObject
-import no.nav.familie.kontrakter.felles.oppgave.Tema
-
 data class JournalposterForBrukerRequest(val brukerId: Bruker,
-                                         val første: Int,
+                                         val antall: Int,
                                          val tema: List<Tema>?=null,
-                                         val journalposttype: List<Journalposttype>?=null) : QueryObject()
+                                         val journalposttype: List<Journalposttype>?=null)

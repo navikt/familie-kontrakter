97e05f945a73e2af35969b0069959532687fff7a
charliemidtlyng <charlie.midtlyng@bekk.no>
2020-08-12 15:28:53 +0200

    Firmaer som selvstendig næringsdrivende skal være en liste, ikke ett element

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Aktivitet.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Aktivitet.kt
@@ -2,7 +2,8 @@ package no.nav.familie.kontrakter.ef.søknad
 
 data class Aktivitet(val hvordanErArbeidssituasjonen: Søknadsfelt<List<String>>,
                      val arbeidsforhold: Søknadsfelt<List<Arbeidsgiver>>? = null,
-                     val selvstendig: Søknadsfelt<Selvstendig>? = null,
+                     @Deprecated("Bruk firmaer istedenfor") val selvstendig: Søknadsfelt<Selvstendig>? = null,
+                     val firmaer: Søknadsfelt<List<Selvstendig>>? = null,
                      val virksomhet: Søknadsfelt<Virksomhet>? = null,
                      val arbeidssøker: Søknadsfelt<Arbeidssøker>? = null,
                      val underUtdanning: Søknadsfelt<UnderUtdanning>? = null,

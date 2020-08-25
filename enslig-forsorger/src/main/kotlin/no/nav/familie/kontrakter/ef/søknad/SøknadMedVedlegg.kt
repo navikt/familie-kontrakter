ecb5be733ce8c1d2f2f1927bf841f2e2a32eae69
Johan Blomgren <johan.blomgren@nav.no>
2020-08-25 12:17:03 +0200

    default tom liste

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/SøknadMedVedlegg.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/SøknadMedVedlegg.kt
@@ -2,4 +2,4 @@ package no.nav.familie.kontrakter.ef.søknad
 
 data class SøknadMedVedlegg<T>(val søknad: T,
                                val vedlegg: List<Vedlegg>,
-                               val dokumentasjonsbehov: List<Dokumentasjonsbehov>?)
+                               val dokumentasjonsbehov: List<Dokumentasjonsbehov> = emptyList())

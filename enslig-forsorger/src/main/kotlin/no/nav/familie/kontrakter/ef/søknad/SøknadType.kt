71f752f3ba3b9951559391be3d6b59700fcad8f9
Martine Enger <marenger@student.matnat.uio.no>
2020-08-26 13:51:32 +0200

    Legger til Dto-objekt for dokumentasjonsbehov og søknadstype

---
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/SøknadType.kt
@@ -0,0 +1,8 @@
+package no.nav.familie.kontrakter.ef.søknad
+
+enum class SøknadType(val dokumentType: String, val dokumentTypeVedlegg: String? = null) {
+    OVERGANGSSTØNAD("OVERGANGSSTØNAD_SØKNAD", "OVERGANGSSTØNAD_SØKNAD_VEDLEGG"),
+    BARNETILSYN("BARNETILSYNSTØNAD_SØKNAD", "BARNETILSYNSTØNAD_VEDLEGG"),
+    SKOLEPENGER("SKOLEPENGER_SØKNAD", "SKOLEPENGER_VEDLEGG"),
+    OVERGANGSSTØNAD_ARBEIDSSØKER("SKJEMA_ARBEIDSSØKER")
+}

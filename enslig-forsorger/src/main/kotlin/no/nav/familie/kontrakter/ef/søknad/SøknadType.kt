f02f061de1c18a5ab49c24bf656d0d49e705fa0d
Martine Enger <marenger@student.matnat.uio.no>
2020-08-27 10:49:32 +0200

    Reverse lookup på søknadstype basert på dokumenttype

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/SøknadType.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/SøknadType.kt
@@ -4,5 +4,12 @@ enum class SøknadType(val dokumentType: String, val dokumentTypeVedlegg: String
     OVERGANGSSTØNAD("OVERGANGSSTØNAD_SØKNAD", "OVERGANGSSTØNAD_SØKNAD_VEDLEGG"),
     BARNETILSYN("BARNETILSYNSTØNAD_SØKNAD", "BARNETILSYNSTØNAD_VEDLEGG"),
     SKOLEPENGER("SKOLEPENGER_SØKNAD", "SKOLEPENGER_VEDLEGG"),
-    OVERGANGSSTØNAD_ARBEIDSSØKER("SKJEMA_ARBEIDSSØKER")
+    OVERGANGSSTØNAD_ARBEIDSSØKER("SKJEMA_ARBEIDSSØKER");
+
+    companion object {
+        private val dokumenttypeMap = values().associateBy(SøknadType::dokumentType)
+        fun hentSøknadTypeForDokumenttype(dokumentType: String): SøknadType {
+            return dokumenttypeMap[dokumentType] ?: throw error("Fant ikke søknadtype for dokumenttype $dokumentType")
+        }
+    }
 }

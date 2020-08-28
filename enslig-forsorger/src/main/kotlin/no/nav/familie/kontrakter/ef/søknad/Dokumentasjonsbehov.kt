e8d7fc72cd38f61ea33b0ada489d7574b4ed998e
Johan Blomgren <johan.blomgren@nav.no>
2020-08-28 14:07:05 +0200

    Fjernet kommentar

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Dokumentasjonsbehov.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Dokumentasjonsbehov.kt
@@ -1,6 +1,6 @@
 package no.nav.familie.kontrakter.ef.søknad
 
 data class Dokumentasjonsbehov(val label: String,
-                               val id: String, // Trenger vi denne? Denne er ju mappet til en tekst i frontend (label)
+                               val id: String,
                                val harSendtInn: Boolean,
                                val opplastedeVedlegg: List<String> = emptyList())

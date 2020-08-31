dfaddd990feca8dffd7d111f47791c2d8bf4f825
Johan Blomgren <blommish@users.noreply.github.com>
2020-08-31 16:02:45 +0200

    Dittnav dokumentasjonsbehov (#173)
    * Lagt inn dokumentasjonsbehov som en del av SøknadMedVedlegg som sendes til mottak
    
    * Legger til Dto-objekt for dokumentasjonsbehov og søknadstype
    
    * Reverse lookup på søknadstype basert på dokumenttype
    
    Co-authored-by: Martine Enger <marenger@student.matnat.uio.no>

---
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/dokumentasjonsbehov/DokumentasjonsbehovDto.kt
@@ -0,0 +1,12 @@
+package no.nav.familie.kontrakter.ef.søknad.dokumentasjonsbehov
+
+import no.nav.familie.kontrakter.ef.søknad.Dokumentasjonsbehov
+import no.nav.familie.kontrakter.ef.søknad.SøknadType
+import java.time.LocalDateTime
+
+data class DokumentasjonsbehovDto(
+    val dokumentasjonsbehov: List<Dokumentasjonsbehov>,
+    val innsendingstidspunkt: LocalDateTime,
+    val søknadType: SøknadType,
+    val personIdent: String
+)

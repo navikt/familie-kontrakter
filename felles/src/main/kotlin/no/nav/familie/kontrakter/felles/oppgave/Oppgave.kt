f6347b27507de0ebd0a5c46452f72d8331db48e8
Nina Kjekstad <nina.kjekstad@nav.no>
2020-09-07 13:50:53 +0200

    Fjern fnr. Ble aldri brukt, da det legges i identer-feltet

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/oppgave/Oppgave.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/oppgave/Oppgave.kt
@@ -6,7 +6,6 @@ import javax.validation.constraints.Pattern
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class Oppgave(val id: Long? = null,
                    val identer: List<OppgaveIdentV2>? = null,
-                   val fnr: String? = null,
                    val tildeltEnhetsnr: String? = null,
                    val endretAvEnhetsnr: String? = null,
                    val eksisterendeOppgaveId: String? = null,

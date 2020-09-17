a6a54401fe0a8d9533d0f484ef01b89ef9d9c6fa
Nina Kjekstad <nina.kjekstad@nav.no>
2020-09-17 13:27:57 +0200

    Lager egen lokal kontrakt med frontend og setter tema manuelt

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/oppgave/FinnOppgaveRequest.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/oppgave/FinnOppgaveRequest.kt
@@ -1,17 +1,11 @@
 package no.nav.familie.kontrakter.felles.oppgave
 
-import com.fasterxml.jackson.core.JsonParser
-import com.fasterxml.jackson.databind.DeserializationContext
-import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer
 import no.nav.familie.kontrakter.felles.abstraction.QueryObject
 import org.springframework.format.annotation.DateTimeFormat
 import java.time.LocalDate
 import java.time.LocalDateTime
 
-data class FinnOppgaveRequest(@JsonDeserialize(using = TemaDeserializer::class)
-                              val tema: Tema,
+data class FinnOppgaveRequest(val tema: Tema,
                               val behandlingstema: Behandlingstema? = null,
                               val oppgavetype: Oppgavetype? = null,
                               val enhet: String? = null,
@@ -33,15 +27,3 @@ data class FinnOppgaveRequest(@JsonDeserialize(using = TemaDeserializer::class)
                               val aktivTomDato: LocalDate? = null,
                               val limit: Long? = null,
                               val offset: Long? = null) : QueryObject()
-
-class TemaDeserializer : StdDeserializer<Tema>(Tema::class.java) {
-    override fun deserialize(jp: JsonParser?, p1: DeserializationContext?): Tema {
-        val node: JsonNode = jp!!.codec.readTree(jp)
-        return when (val tema = node.asText()) {
-            "BAR" -> Tema.BAR
-            "ENF" -> Tema.ENF
-            "KON" -> Tema.KON
-            else -> Tema.valueOf(tema)
-        }
-    }
-}

77124e43e1e2bd5979ebab7b69cf0cab08620cb7
Nina Kjekstad <ninakjek@users.noreply.github.com>
2020-09-22 15:57:47 +0200

    Legg til flere oppgavetyper (#185)
    * Legg til fler eoppgavetyper
    
    * Legg til trailing comma og formater

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/oppgave/OpprettOppgaveRequest.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/oppgave/OpprettOppgaveRequest.kt
@@ -3,25 +3,40 @@ package no.nav.familie.kontrakter.felles.oppgave
 import java.time.LocalDate
 
 data class OpprettOppgaveRequest(val ident: OppgaveIdentV2?,
-                          val enhetsnummer: String?,
-                          val saksId: String?,
-                          val journalpostId: String? = null,
-                          val tema: Tema,
-                          val oppgavetype: Oppgavetype,
-                          val behandlingstema: String?,
-                          val tilordnetRessurs: String? = null,
-                          val fristFerdigstillelse: LocalDate,
-                          val aktivFra: LocalDate = LocalDate.now(),
-                          val beskrivelse: String,
-                          val prioritet: OppgavePrioritet = OppgavePrioritet.NORM,
-                          val behandlingstype: String? = null)
+                                 val enhetsnummer: String?,
+                                 val saksId: String?,
+                                 val journalpostId: String? = null,
+                                 val tema: Tema,
+                                 val oppgavetype: Oppgavetype,
+                                 val behandlingstema: String?,
+                                 val tilordnetRessurs: String? = null,
+                                 val fristFerdigstillelse: LocalDate,
+                                 val aktivFra: LocalDate = LocalDate.now(),
+                                 val beskrivelse: String,
+                                 val prioritet: OppgavePrioritet = OppgavePrioritet.NORM,
+                                 val behandlingstype: String? = null)
 
 enum class Oppgavetype(val value: String) {
     BehandleSak("BEH_SAK"),
     Journalføring("JFR"),
     GodkjenneVedtak("GOD_VED"),
     BehandleUnderkjentVedtak("BEH_UND_VED"),
-    Fordeling("FDR")
+    Fordeling("FDR"),
+    BehandleReturpost("RETUR"),
+    BehandleSED("BEH_SED"),
+    BehandleUderkjentVedtak("BEH_UND_VED"),
+    FordelingSED("FDR_SED"),
+    Fremlegg("FREM"),
+    Generell("GEN"),
+    InnhentDokumentasjon("INNH_DOK"),
+    JournalføringUtgående("JFR_UT"),
+    KontaktBruker("KONT_BRUK"),
+    KontrollerUtgåendeSkannetDokument("KON_UTG_SCA_DOK"),
+    SvarIkkeMottatt("SVAR_IK_MOT"),
+    VurderDokument("VUR"),
+    VurderHenvendelse("VURD_HENV"),
+    VurderKonsekvensForYtelse("VUR_KONS_YTE"),
+    VurderSvar("VUR_SVAR"),
 }
 
 enum class Behandlingstema(val value: String) {

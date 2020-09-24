d88934a00376769c1a0eeae602a97b44572b54f3
bent-lorentzen <54444412+bent-lorentzen@users.noreply.github.com>
2020-09-24 14:18:04 +0200

    OpprettInfotrygdSakRequest og response (#188)
    * OpprettInfotrygdSakRequest og response

---
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/infotrygdsak/OpprettInfotrygdSakRequest.kt
@@ -0,0 +1,16 @@
+package no.nav.familie.kontrakter.felles.infotrygdsak
+
+import java.time.LocalDate
+
+class OpprettInfotrygdSakRequest(var fnr: String,
+                                 var fagomrade: String,
+                                 var stonadsklassifisering2: String? = null,
+                                 var stonadsklassifisering3: String? = null,
+                                 var type: String? = null,
+                                 var opprettetAv: String? = null,
+                                 var opprettetAvOrganisasjonsEnhetsId: String? = null,
+                                 var mottakerOrganisasjonsEnhetsId: String? = null,
+                                 var motattdato: LocalDate? = null,
+                                 var sendBekreftelsesbrev: Boolean? = null,
+                                 var oppgaveId: String? = null,
+                                 var oppgaveOrganisasjonsenhetId: String? = null)

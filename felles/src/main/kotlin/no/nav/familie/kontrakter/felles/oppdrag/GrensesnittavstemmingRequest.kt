0e8004ba423a752dae9cd4cb58a6090c18d6f78c
bent-lorentzen <54444412+bent-lorentzen@users.noreply.github.com>
2020-11-04 12:56:52 +0100

    Lagt til kontrakter for avstemming. (#202)

---
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/oppdrag/GrensesnittavstemmingRequest.kt
@@ -0,0 +1,7 @@
+package no.nav.familie.kontrakter.felles.oppdrag
+
+import java.time.LocalDateTime
+
+data class GrensesnittavstemmingRequest(val fagsystem: String,
+                                        val fra: LocalDateTime,
+                                        val til: LocalDateTime)

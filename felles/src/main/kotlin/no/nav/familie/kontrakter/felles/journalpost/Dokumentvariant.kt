832d535af9a6011720dda07b3c97a90207becf0f
charliemidtlyng <charlie.midtlyng@bekk.no>
2020-11-10 15:31:02 +0100

    Skal hente filnavn nfor dokumenter når journalpost hentes ut

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/journalpost/Dokumentvariant.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/journalpost/Dokumentvariant.kt
@@ -1,3 +1,3 @@
 package no.nav.familie.kontrakter.felles.journalpost
 
-data class Dokumentvariant(val variantformat: String)
+data class Dokumentvariant(val variantformat: String, val filnavn: String? = null)

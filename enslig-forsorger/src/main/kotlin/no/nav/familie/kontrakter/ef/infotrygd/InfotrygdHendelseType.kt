96ddf6b515894526e42db02aeed8c61ce268e33a
Johan Blomgren <johan.blomgren@nav.no>
2020-11-20 11:08:36 +0100

    Lagt til kontrakt for å opprette hendelser i familie-ef-infotrygd-feed

---
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/infotrygd/InfotrygdHendelseType.kt
@@ -0,0 +1,12 @@
+package no.nav.familie.kontrakter.ef.infotrygd
+
+enum class InfotrygdHendelseType {
+    EF_Vedtak_Skolepenger,
+    EF_StartBeh_Skolepenger,
+
+    EF_Vedtak_OvergStoenad,
+    EF_StartBeh_OvergStoenad,
+
+    EF_Vedtak_Barnetilsyn,
+    EF_StartBeh_Barnetilsyn;
+}

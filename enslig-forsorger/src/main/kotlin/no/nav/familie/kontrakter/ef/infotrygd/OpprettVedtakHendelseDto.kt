46fb8fb77b6356eb8d5402f271b400d0ade97de9
Johan Blomgren <blommish@users.noreply.github.com>
2020-12-03 14:20:41 +0100

    Infotrygd feed v2 (#232)
    * Endret på kontraktet mot infotrygd-feed
    
    * Slettet periode i denne runde

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/infotrygd/OpprettVedtakHendelseDto.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/infotrygd/OpprettVedtakHendelseDto.kt
@@ -3,5 +3,6 @@ package no.nav.familie.kontrakter.ef.infotrygd
 import java.time.LocalDate
 
 data class OpprettVedtakHendelseDto(val fnr: String,
-                                    val type: InfotrygdHendelseType,
+                                    val saksnummer: Int,
+                                    val type: StønadType,
                                     val startdato: LocalDate)

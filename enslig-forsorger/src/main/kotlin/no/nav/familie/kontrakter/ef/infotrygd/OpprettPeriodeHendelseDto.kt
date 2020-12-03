4105782719fa072687788e533cc8ccafd2f93c86
Johan Blomgren <johan.blomgren@nav.no>
2020-12-03 12:22:03 +0100

    Slettet periode i denne runde

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/infotrygd/OpprettPeriodeHendelseDto.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.familie.kontrakter.ef.infotrygd
-
-import java.time.LocalDate
-
-data class OpprettPeriodeHendelseDto(val fnr: String,
-                                     val saksnummer: Int,
-                                     val type: StønadType,
-                                     val startdato: LocalDate,
-                                     val sluttdato: LocalDate)

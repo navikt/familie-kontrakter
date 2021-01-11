ec506e06ee83efd3cf52e63b73d2daec82a3e0f5
eirarset <eirik.arseth@nav.no>
2021-01-11 09:16:47 +0100

    Legge r på svarid i søknadsfelt

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Søknadsfelt.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Søknadsfelt.kt
@@ -1,3 +1,5 @@
 package no.nav.familie.kontrakter.ef.søknad
 
-data class Søknadsfelt<T>(val label: String, val verdi: T, val alternativer: List<String>? = null)
+data class Søknadsfelt<T>(val label: String, val verdi: T, val alternativer: List<String>? = null, val svarid: T? = null)
+
+data class EnumTekstverdiMedSvarid(val verdi: String, val svarid: String)

23882584b8e31ce448cb8c3e179f595ad1bcbc76
Saikat Halder <saikat.halder@nav.no>
2021-01-12 17:27:36 +0100

    TEA-3338 WIP endret noen variable navn og fikset review kommentar

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/tilbakekreving/OpprettTilbakekrevingRequest.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/tilbakekreving/OpprettTilbakekrevingRequest.kt
@@ -2,13 +2,13 @@ package no.nav.familie.kontrakter.felles.tilbakekreving
 
 import no.nav.familie.kontrakter.felles.PersonIdent
 
-data class OpprettTilbakekrevingRequest(val fagsytem: String,
+data class OpprettTilbakekrevingRequest(val fagsystem: String,
                                         val eksternFagsakId: String,
                                         val personIdent: PersonIdent,
-                                        val aktoerId: Long, // Brukes aktoerId kanskje istedenfor personIdent??
+                                        val aktørId: Long, // Brukes aktoerId kanskje istedenfor personIdent??
                                         val henvisning: String, // BehandlingId/UUID til BA/EF/KA
                                         val behandlingType: BehandlingType,
-                                        val spraakKode: String? = "NB",
+                                        val språkkode: String? = "NB",
                                         val enhetId: String,
-                                        val enhetNavn: String,
+                                        val enhetsnavn: String,
                                         val verge: Verge? = null)

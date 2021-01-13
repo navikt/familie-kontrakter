0f240aa36b48b42a24fd0441b07a686f6f21c636
Saikat Halder <saikat.halder@nav.no>
2021-01-13 13:48:10 +0100

    TEA-3338 WIP inkludere varselTekst, feilutbetalingPerioder felt

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/tilbakekreving/OpprettTilbakekrevingRequest.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/tilbakekreving/OpprettTilbakekrevingRequest.kt
@@ -1,14 +1,17 @@
 package no.nav.familie.kontrakter.felles.tilbakekreving
 
 import no.nav.familie.kontrakter.felles.PersonIdent
+import java.time.LocalDate
 
 data class OpprettTilbakekrevingRequest(val fagsystem: String,
                                         val eksternFagsakId: String,
                                         val personIdent: PersonIdent,
-                                        val aktørId: Long, // Brukes aktoerId kanskje istedenfor personIdent??
                                         val henvisning: String, // BehandlingId/UUID til BA/EF/KA
                                         val behandlingType: BehandlingType,
                                         val språkkode: String? = "NB",
                                         val enhetId: String,
                                         val enhetsnavn: String,
+                                        val varselTekst: String,
+                                        val feilutbetaltePerioder: FeilutbetaltePerioder,
+                                        val revurderingVedtakDato:LocalDate,
                                         val verge: Verge? = null)

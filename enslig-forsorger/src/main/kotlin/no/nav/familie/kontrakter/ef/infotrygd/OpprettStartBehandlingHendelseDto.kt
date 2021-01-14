f7b48c5413ef98241179f4a9118b53f225497dc7
Johan Blomgren <johan.blomgren@nav.no>
2021-01-14 09:58:05 +0100

    Infotrygd ønsker att vi oppretter hendelser for hver ident av en person

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/infotrygd/OpprettStartBehandlingHendelseDto.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/infotrygd/OpprettStartBehandlingHendelseDto.kt
@@ -2,5 +2,8 @@ package no.nav.familie.kontrakter.ef.infotrygd
 
 import no.nav.familie.kontrakter.ef.felles.StønadType
 
-data class OpprettStartBehandlingHendelseDto(val fnr: String,
+/**
+ * @param personIdenter alle identer til personen
+ */
+data class OpprettStartBehandlingHendelseDto(val personIdenter: Set<String>,
                                              val type: StønadType)

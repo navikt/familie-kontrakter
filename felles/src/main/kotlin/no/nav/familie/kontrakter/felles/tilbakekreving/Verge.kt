a54cac4880b064570a92ceaa87e6bcef898e3895
Saikat Halder <saikat.halder@nav.no>
2021-01-27 13:55:57 +0100

    La til validering for tilbakekreving opprettelse kontrakt

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/tilbakekreving/Verge.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/tilbakekreving/Verge.kt
@@ -3,13 +3,13 @@ package no.nav.familie.kontrakter.felles.tilbakekreving
 import no.nav.familie.kontrakter.felles.PersonIdent
 import java.time.LocalDate
 import javax.validation.Valid
-import javax.validation.constraints.Size
+import javax.validation.constraints.Pattern
 
 data class Verge(val vergeType: VergeType,
                  val gyldigFom: LocalDate,
                  val gyldigTom: LocalDate,
                  val navn: String,
-                 @field:Size(max = 9,message = "Organisasjonsnummer er for stor")
+                 @field:Pattern(regexp= "(^$|.{9})", message = "Organisasjonsnummer er ikke riktig")
                  val organisasjonsnummer: String? = null,
                  @field:Valid
                  val personIdent: PersonIdent? = null)

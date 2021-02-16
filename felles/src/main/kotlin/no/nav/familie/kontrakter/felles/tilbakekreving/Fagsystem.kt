1dddd070a86dd62228bac62291ae57b77173bcac
Saikat Halder <saikat.halder@nav.no>
2021-02-16 15:35:26 +0100

    la til ytelsestype kode i Ytelsestype

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/tilbakekreving/Fagsystem.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/tilbakekreving/Fagsystem.kt
@@ -4,17 +4,4 @@ enum class Fagsystem(val navn: String, val tema: String) {
     BA("BARNETRYGD", "BAR"),
     EF("ENSLIG FORELDER", "ENF"),
     KS("KONTANTSTØTTE", "KON");
-
-    companion object {
-
-        fun fraYtelsestype(type: Ytelsestype): Fagsystem {
-            return when (type) {
-                Ytelsestype.BARNETRYGD -> BA
-                Ytelsestype.KONTANTSTØTTE -> KS
-                Ytelsestype.OVERGANGSSTØNAD -> EF
-                Ytelsestype.BARNETILSYN -> EF
-                Ytelsestype.SKOLEPENGER -> EF
-            }
-        }
-    }
 }
--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/tilbakekreving/Ytelsestype.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/tilbakekreving/Ytelsestype.kt
@@ -1,9 +1,9 @@
 package no.nav.familie.kontrakter.felles.tilbakekreving
 
-enum class Ytelsestype {
-    BARNETRYGD,
-    OVERGANGSSTØNAD,
-    BARNETILSYN,
-    SKOLEPENGER,
-    KONTANTSTØTTE
+enum class Ytelsestype(val kode: String) {
+    BARNETRYGD("BA"),
+    OVERGANGSSTØNAD("OG"),
+    BARNETILSYN("BT"),
+    SKOLEPENGER("SP"),
+    KONTANTSTØTTE("KS")
 }

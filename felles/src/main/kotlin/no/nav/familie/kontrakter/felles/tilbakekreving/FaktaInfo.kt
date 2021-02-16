7c849c4afc46a5e074a5f1a1f2e7d77b5fbf83ce
Saikat Halder <saikat.halder@nav.no>
2021-02-16 14:22:55 +0100

    endret revurderingsårsaker til Set<String>

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/tilbakekreving/FaktaInfo.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/tilbakekreving/FaktaInfo.kt
@@ -1,7 +1,7 @@
 package no.nav.familie.kontrakter.felles.tilbakekreving
 
 data class FaktaInfo(
-        val revurderingsårsak: String,
+        val revurderingsårsaker: Set<String>,
         val revurderingsresultat: String,
         val tilbakekrevingsvalg: Tilbakekrevingsvalg,
         val konsekvensForYtelser: Set<String>? = emptySet()

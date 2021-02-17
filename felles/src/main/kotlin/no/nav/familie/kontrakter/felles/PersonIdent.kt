4caad78a92e2162438df38754e3661f39e9c171c
Bent J. Lorentzen <bent.lorentzen@nav.no>
2021-02-17 10:48:55 +0100

    Endret for å støtte brev.

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/PersonIdent.kt
+++
@@ -1,5 +0,0 @@
-package no.nav.familie.kontrakter.felles
-
-import javax.validation.constraints.Pattern
-
-data class PersonIdent(@field:Pattern(regexp= "(^$|.{11})", message = "PersonIdent er ikke riktig") val ident: String)

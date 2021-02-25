26267a81b53ccc371c90a08e277814c48b3ce710
Bent J. Lorentzen <bent.lorentzen@nav.no>
2021-02-25 15:06:20 +0100

    Lagt tilbake PersonIdent.kt

---
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/PersonIdent.kt
@@ -0,0 +1,5 @@
+package no.nav.familie.kontrakter.felles
+
+import javax.validation.constraints.Pattern
+
+data class PersonIdent(@field:Pattern(regexp = "(^$|.{11})", message = "PersonIdent er ikke riktig") val ident: String)

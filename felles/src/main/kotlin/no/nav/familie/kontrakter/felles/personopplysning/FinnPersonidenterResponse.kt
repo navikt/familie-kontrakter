8150fb637830d590ee5154ca11ecac05389ff743
Johan Blomgren <blommish@users.noreply.github.com>
2021-03-03 14:52:35 +0100

    Legger til dto for å finne personidenter til en person

---
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/personopplysning/FinnPersonidenterResponse.kt
@@ -0,0 +1,5 @@
+package no.nav.familie.kontrakter.felles.personopplysning
+
+data class FinnPersonidenterResponse(val identer: List<PersonIdentMedHistorikk>)
+
+data class PersonIdentMedHistorikk(val personIdent: String, val historisk: Boolean)

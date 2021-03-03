f0cc1961ec204e0d9fadf065712e10916a65c351
Johan Blomgren <blommish@users.noreply.github.com>
2021-03-03 13:13:41 +0100

    Lagt til nye dtoer mot infotrygd for å sjekke om man finnes i infotrygd, som returnerer metadata om hvilken fnr som har treff (#303)

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/infotrygd/InfotrygdSøkRequest.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/infotrygd/InfotrygdSøkRequest.kt
@@ -2,8 +2,14 @@ package no.nav.familie.kontrakter.ef.infotrygd
 
 import no.nav.familie.kontrakter.ef.felles.StønadType
 
+@Deprecated("bruk InfotrygdSøkRequest")
 /**
  * @param personIdenter alle identer til personen
  */
 data class SøkFlereStønaderRequest(val personIdenter: Set<String>,
                                    val stønader: Set<StønadType>)
+
+/**
+ * @param personIdenter alle identer til personen
+ */
+data class InfotrygdSøkRequest(val personIdenter: Set<String>)

4097ceed1215dd11aa691bdd8eb4110c2bfb293f
Johan Blomgren <blommish@users.noreply.github.com>
2021-04-16 13:51:54 +0200

    behandleINySaksbehandling er litt enklere å skjønne enn skalAutomatiskJournalføres

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/SøknadMedVedlegg.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/SøknadMedVedlegg.kt
@@ -3,4 +3,4 @@ package no.nav.familie.kontrakter.ef.søknad
 data class SøknadMedVedlegg<T>(val søknad: T,
                                val vedlegg: List<Vedlegg>,
                                val dokumentasjonsbehov: List<Dokumentasjonsbehov> = emptyList(),
-                               val skalAutomatiskJournalføres: Boolean = true)
+                               val behandleINySaksbehandling: Boolean = false)

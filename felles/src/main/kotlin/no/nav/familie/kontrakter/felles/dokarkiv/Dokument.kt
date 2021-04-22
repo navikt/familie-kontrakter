077b07f24f32bd8e5f7ab9597d81af6a8803be99
Bent J. Lorentzen <bent.lorentzen@nav.no>
2021-04-22 13:17:52 +0200

    Byttet rekkefølge på felter for å minske differanse med gammel dto.

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/dokarkiv/Dokument.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/dokarkiv/Dokument.kt
@@ -2,6 +2,7 @@ package no.nav.familie.kontrakter.felles.dokarkiv
 
 import javax.validation.constraints.NotEmpty
 
+@Deprecated("Bruk v2")
 class Dokument(@field:NotEmpty val dokument: ByteArray,
                @field:NotEmpty val filType: FilType,
                val filnavn: String? = null,
--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/dokarkiv/v2/ArkiverDokumentRequest.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/dokarkiv/v2/ArkiverDokumentRequest.kt
@@ -5,14 +5,14 @@ import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotEmpty
 
 data class ArkiverDokumentRequest(@field:NotBlank val fnr: String,
-                                  val avsenderMottaker: AvsenderMottaker?,
                                   val forsøkFerdigstill: Boolean,
                                   @field:NotEmpty val hoveddokumentvarianter: List<Dokument>,
                                   val vedleggsdokumenter: List<Dokument> = emptyList(),
-                                  val eksternReferanseId: String? = null,
                                   val fagsakId: String? = null,
         //Skal ikke settes for innkommende hvis Ruting gjøres av BRUT001
                                   val journalførendeEnhet: String? = null,
         //Ønsker du å generer forside når du tilskriver bruker kan du fylle ut denne
-                                  val førsteside: Førsteside? = null)
+                                  val førsteside: Førsteside? = null,
+                                  val eksternReferanseId: String? = null,
+                                  val avsenderMottaker: AvsenderMottaker? = null)
 

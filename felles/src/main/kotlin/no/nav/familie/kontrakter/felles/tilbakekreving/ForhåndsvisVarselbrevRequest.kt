8fa83670277813d3a7a29df1dad36b757c315e15
Nina Kjekstad <nina.kjekstad@nav.no>
2021-04-27 08:53:26 +0200

    Legg til request-dto for forhåndsvisvarselbrev-endepunkt

---
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/tilbakekreving/ForhåndsvisVarselbrevRequest.kt
@@ -0,0 +1,22 @@
+package no.nav.familie.kontrakter.felles.tilbakekreving
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import java.time.LocalDate
+import javax.validation.constraints.Size
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class ForhåndsvisVarselbrevRequest(@Size(max = 1500, message = "Varseltekst er for lang")
+                                        val varseltekst: String? = null,
+                                        val ytelsestype: Ytelsestype,
+                                        val behandlendeEnhetId: String? = null,
+                                        val behandlendeEnhetsNavn: String,
+                                        val språkkode: Språkkode = Språkkode.NB,
+                                        val vedtaksdato: LocalDate? = null,
+                                        val feilutbetaltePerioderDto: FeilutbetaltePerioderDto,
+                                        val fagsystem: Fagsystem,
+                                        val saksnummer: String,
+                                        val ident: String,
+                                        val verge: Verge? = null)
+
+class FeilutbetaltePerioderDto(var sumFeilutbetaling: Long,
+                               var perioder: List<Periode>)

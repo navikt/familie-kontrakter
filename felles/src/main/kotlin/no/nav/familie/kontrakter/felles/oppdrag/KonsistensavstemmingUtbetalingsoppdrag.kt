095e1aa302b51af3f0293bd7a45dd65d5d9bc188
Johan Blomgren <blommish@users.noreply.github.com>
2021-05-18 16:58:04 +0200

    Fjernet konsistensavstemming v1 som ikke brukes og lagt til en ny versjon med utbetalingsoppdrag slik att vi faktiskt konsistensavstemmer att vi har riktig beløp i saksbehandling og økonomi

---
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/oppdrag/KonsistensavstemmingUtbetalingsoppdrag.kt
@@ -0,0 +1,7 @@
+package no.nav.familie.kontrakter.felles.oppdrag
+
+import java.time.LocalDateTime
+
+data class KonsistensavstemmingUtbetalingsoppdrag(val fagsystem: String,
+                                                  val utbetalingsoppdrag: List<Utbetalingsoppdrag>,
+                                                  val avstemmingstidspunkt: LocalDateTime)

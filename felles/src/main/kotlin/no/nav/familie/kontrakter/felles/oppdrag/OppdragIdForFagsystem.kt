0d0915c6c6f47b87d2d90dfd0946ea38067cb8ce
Johan Blomgren <blommish@users.noreply.github.com>
2021-05-25 08:31:20 +0200

    Oppdrag konsistensavstemming med utbetalingsoppdrag (#349)
    * Fjernet konsistensavstemming v1 som ikke brukes og lagt til en ny versjon med utbetalingsoppdrag slik att vi faktiskt konsistensavstemmer att vi har riktig beløp i saksbehandling og økonomi
    
    * Fjerner OppdragIdForFagsystem.kt også

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/oppdrag/OppdragIdForFagsystem.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.familie.kontrakter.felles.oppdrag
-
-@Deprecated(message = "Trenger å sende med periodeIdn",
-            replaceWith = ReplaceWith("no.nav.familie.kontrakter.felles.oppdrag.OppdragIdForFagsystemV2"))
-data class OppdragIdForFagsystem(val personIdent: String,
-                                 val behandlingsId: Long) {
-    override fun toString(): String = "OppdragId(behandlingsId=$behandlingsId)"
-}

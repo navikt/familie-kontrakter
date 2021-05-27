8140e14a3ad3a8fc23604cca04ec409cc21e1612
Trond Lillesand <56258085+throndi@users.noreply.github.com>
2021-05-27 15:03:13 +0200

    Rettelse (fjerner TilkjentYtelseStatus ut fra ef.iverksett, da denne flyttet til ef.felles)

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/iverksett/TilkjentYtelseDto.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/iverksett/TilkjentYtelseDto.kt
@@ -4,12 +4,5 @@ data class TilkjentYtelseDto(
         val andelerTilkjentYtelse: List<AndelTilkjentYtelseDto>
 )
 
-enum class TilkjentYtelseStatus {
-    IKKE_KLAR,
-    OPPRETTET,
-    SENDT_TIL_IVERKSETTING,
-    AKTIV,
-    AVSLUTTET
-}
 
 

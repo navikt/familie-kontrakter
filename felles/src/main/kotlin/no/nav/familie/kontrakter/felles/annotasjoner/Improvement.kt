da8aa34c32da73666f15ebdc11de989800f76324
charliemidtlyng <charlie.midtlyng@bekk.no>
2021-06-02 15:14:40 +0200

    Endret på Iverksett til å passe for EF-saks behov

---
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/annotasjoner/Improvement.kt
@@ -0,0 +1,7 @@
+package no.nav.familie.kontrakter.felles.annotasjoner
+
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION,
+        AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.EXPRESSION)
+@Retention(AnnotationRetention.SOURCE)
+@MustBeDocumented
+annotation class Improvement(val message: String = "")

2a7fd402a75587252fabe06a6bdc104163a74547
Geir Roger Moen <geir.roger.moen@nav.no>
2021-06-10 10:14:25 +0200

    TEA-4640 Dto for tilbakekrevingsbehandling for visning i saksoversikten i fagsystemet

---
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/tilbakekreving/Behandling.kt
@@ -0,0 +1,37 @@
+package no.nav.familie.kontrakter.felles.tilbakekreving
+
+import java.time.LocalDateTime
+import java.util.UUID
+
+class Behandling(val behandlingId: UUID,
+                 val opprettetTidspunkt: LocalDateTime,
+                 val aktiv: Boolean,
+                 val årsak: Behandlingsårsakstype?,
+                 val type: Behandlingstype,
+                 val status: Behandlingsstatus,
+                 val vedtaksdato: LocalDateTime?,
+                 val resultat: Behandlingsresultatstype?)
+
+enum class Behandlingsårsakstype {
+    REVURDERING_KLAGE_NFP,
+    REVURDERING_KLAGE_KA,
+    REVURDERING_OPPLYSNINGER_OM_VILKÅR,
+    REVURDERING_OPPLYSNINGER_OM_FORELDELSE,
+    REVURDERING_FEILUTBETALT_BELØP_HELT_ELLER_DELVIS_BORTFALT,
+}
+
+enum class Behandlingsstatus {
+    AVSLUTTET,
+    FATTER_VEDTAK,
+    IVERKSETTER_VEDTAK,
+    OPPRETTET,
+    UTREDES,
+}
+
+enum class Behandlingsresultatstype {
+    IKKE_FASTSATT,
+    INGEN_TILBAKEBETALING,
+    DELVIS_TILBAKEBETALING,
+    FULL_TILBAKEBETALING,
+    HENLAGT,
+}

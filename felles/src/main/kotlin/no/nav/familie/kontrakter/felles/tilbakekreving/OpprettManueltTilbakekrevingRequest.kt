2ff8dd7cc84f7b6096c99978efba95e41872e44a
haldersaikat <saikat.halder@nav.no>
2021-06-25 12:35:31 +0200

    La til respons DTO i familie-kontrakt. Den skal brukes for å returnere /kanBehandlingOpprettesManuelt/v1 tjeneste respons fra familie-tilbake

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/tilbakekreving/OpprettManueltTilbakekrevingRequest.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/tilbakekreving/OpprettManueltTilbakekrevingRequest.kt
@@ -5,3 +5,8 @@ data class OpprettManueltTilbakekrevingRequest(val eksternFagsakId: String,
                                                val ytelsestype: Ytelsestype,
         // Fagsystemreferanse til behandlingen, må være samme id som brukes mot datavarehus og økonomi
                                                val eksternId: String)
+
+// familie-tilbake bruker denne dto-en til å sende respons på ../kanBehandlingOpprettesManuelt/v1 tjeneste
+data class KanBehandlingOpprettesManueltRespons(val kanBehandlingOpprettes: Boolean,
+                                                val melding: String,
+                                                val kravgrunnlagsreferanse: String? = null)

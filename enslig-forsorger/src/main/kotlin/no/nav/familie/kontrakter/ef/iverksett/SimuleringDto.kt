d68a3c49b97a3350bd3b1bb66ae7b0606c756e17
Neringa Altanaite <neringa.altanaite@nav.no>
2021-07-01 11:04:56 +0200

    lager simuleringDto

---
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/iverksett/SimuleringDto.kt
@@ -0,0 +1,26 @@
+package no.nav.familie.kontrakter.ef.iverksett
+
+import no.nav.familie.kontrakter.ef.felles.StønadType
+import java.time.LocalDate
+import java.util.UUID
+
+data class SimuleringDto(
+        val nyTilkjentYtelseMedMetaData: TilkjentYtelseMedMetadata,
+        val forrigeBehandlingId: UUID?
+)
+
+data class TilkjentYtelseMedMetadata(
+        val tilkjentYtelse: SimuleringTilkjentYtelseDto,
+        val saksbehandlerId: String,
+        val eksternBehandlingId: Long,
+        val stønadstype: StønadType,
+        val eksternFagsakId: Long,
+        val personIdent: String,
+        val behandlingId: UUID,
+        val vedtaksdato: LocalDate)
+
+data class SimuleringTilkjentYtelseDto(
+        val id: UUID = UUID.randomUUID(),
+        val vedtaksdato: LocalDate? = null,
+        val andelerTilkjentYtelse: List<AndelTilkjentYtelseDto>)
+

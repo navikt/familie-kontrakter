058d535d85b7ae96789f06d0c1e963b70cbab17b
eirarset <eirik.arseth@nav.no>
2021-07-15 12:21:39 +0200

    Formatering

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/ettersending/SøknadMedDokumentasjonsbehovDto.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/ettersending/SøknadMedDokumentasjonsbehovDto.kt
@@ -4,4 +4,7 @@ import no.nav.familie.kontrakter.ef.felles.StønadType
 import no.nav.familie.kontrakter.ef.søknad.dokumentasjonsbehov.DokumentasjonsbehovDto
 import java.time.LocalDate
 
-data class SøknadMedDokumentasjonsbehovDto(val søknadId: String, val stønadType: StønadType, val søknadDato: LocalDate, val dokumentasjonsbehov: DokumentasjonsbehovDto)
+data class SøknadMedDokumentasjonsbehovDto(val søknadId: String,
+                                           val stønadType: StønadType,
+                                           val søknadDato: LocalDate,
+                                           val dokumentasjonsbehov: DokumentasjonsbehovDto)

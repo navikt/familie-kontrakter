6df8dd8a43707fa964f8e1511a32477bc402d946
Simen <simentengs@gmail.com>
2021-07-19 13:09:27 +0200

    slettet EttersendingMedVedlegg fra søknad

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/ettersending/Ettersending.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/ettersending/Ettersending.kt
@@ -1,9 +1,8 @@
 package no.nav.familie.kontrakter.ef.ettersending
 
 import no.nav.familie.kontrakter.ef.søknad.Innsendingsdetaljer
-import no.nav.familie.kontrakter.ef.søknad.Personalia
 import no.nav.familie.kontrakter.ef.søknad.Søknadsfelt
 
-data class Ettersending(val personalia: Søknadsfelt<Personalia>,
+data class Ettersending(val fnr: String,
                         val innsendingsdetaljer: Søknadsfelt<Innsendingsdetaljer>
                       )
--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/ettersending/EttersendingMedVedlegg.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/ettersending/EttersendingMedVedlegg.kt
@@ -3,7 +3,7 @@ package no.nav.familie.kontrakter.ef.ettersending
 import no.nav.familie.kontrakter.ef.søknad.Dokumentasjonsbehov
 import no.nav.familie.kontrakter.ef.søknad.Vedlegg
 
-data class EttersendingMedVedlegg<T>(val ettersending: T,
-                                     val vedlegg: List<Vedlegg>,
+data class EttersendingMedVedlegg<T>(val ettersending: Ettersending,
+                                     val vedlegg: List<Vedlegg> = emptyList(),
                                      val dokumentasjonsbehov: List<Dokumentasjonsbehov> = emptyList(),
                                      val behandleINySaksbehandling: Boolean = false)

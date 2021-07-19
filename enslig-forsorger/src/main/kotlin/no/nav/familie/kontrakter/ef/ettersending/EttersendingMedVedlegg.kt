42d04e5ff79f093bb91adad25d4d589984570282
Simen <simentengs@gmail.com>
2021-07-19 15:30:42 +0200

    fjernet <T> fra EttersendingMedVedlegg

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/ettersending/EttersendingMedVedlegg.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/ettersending/EttersendingMedVedlegg.kt
@@ -3,7 +3,7 @@ package no.nav.familie.kontrakter.ef.ettersending
 import no.nav.familie.kontrakter.ef.søknad.Dokumentasjonsbehov
 import no.nav.familie.kontrakter.ef.søknad.Vedlegg
 
-data class EttersendingMedVedlegg<T>(val ettersending: Ettersending,
+data class EttersendingMedVedlegg(val ettersending: Ettersending,
                                      val vedlegg: List<Vedlegg> = emptyList(),
                                      val dokumentasjonsbehov: List<Dokumentasjonsbehov> = emptyList())
 

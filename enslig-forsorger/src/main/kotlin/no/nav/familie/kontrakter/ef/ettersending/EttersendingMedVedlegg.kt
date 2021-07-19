523d37c83c3fc98c3f567b0bdfbb7130fc8100b4
Simen <simentengs@gmail.com>
2021-07-19 15:32:17 +0200

    formater

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/ettersending/EttersendingMedVedlegg.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/ettersending/EttersendingMedVedlegg.kt
@@ -3,7 +3,9 @@ package no.nav.familie.kontrakter.ef.ettersending
 import no.nav.familie.kontrakter.ef.søknad.Dokumentasjonsbehov
 import no.nav.familie.kontrakter.ef.søknad.Vedlegg
 
-data class EttersendingMedVedlegg(val ettersending: Ettersending,
-                                     val vedlegg: List<Vedlegg> = emptyList(),
-                                     val dokumentasjonsbehov: List<Dokumentasjonsbehov> = emptyList())
+data class EttersendingMedVedlegg(
+    val ettersending: Ettersending,
+    val vedlegg: List<Vedlegg> = emptyList(),
+    val dokumentasjonsbehov: List<Dokumentasjonsbehov> = emptyList()
+)
 

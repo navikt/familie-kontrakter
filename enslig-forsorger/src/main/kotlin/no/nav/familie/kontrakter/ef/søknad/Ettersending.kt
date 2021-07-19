d5030ea1e518f84d24ec247c58e8c4d8e8255369
Simen <simentengs@gmail.com>
2021-07-19 13:35:02 +0200

    sletter ettersending fra søknad

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Ettersending.kt
+++
@@ -1,5 +0,0 @@
-package no.nav.familie.kontrakter.ef.søknad
-
-data class Ettersending(  val fnr: String,
-                          val innsendingsdetaljer: Søknadsfelt<Innsendingsdetaljer>
-                      )

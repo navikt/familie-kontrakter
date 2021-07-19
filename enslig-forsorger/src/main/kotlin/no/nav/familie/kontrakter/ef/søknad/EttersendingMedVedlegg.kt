243585a4c3e47106503f82db18c1ff0024e6587e
Simen <simentengs@gmail.com>
2021-07-19 13:09:21 +0200

    slettet EttersendingMedVedlegg fra søknad

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/EttersendingMedVedlegg.kt
+++
@@ -1,6 +0,0 @@
-package no.nav.familie.kontrakter.ef.søknad
-
-data class EttersendingMedVedlegg<T>(val ettersending: Ettersending,
-                               val vedlegg: List<Vedlegg> = emptyList(),
-                               val dokumentasjonsbehov: List<Dokumentasjonsbehov> = emptyList(),
-                               val behandleINySaksbehandling: Boolean = false)

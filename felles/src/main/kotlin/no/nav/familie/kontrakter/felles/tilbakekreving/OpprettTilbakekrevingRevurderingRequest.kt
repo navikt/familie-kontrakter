90894cad6d083dd76c4690f560ad005c671fe040
haldersaikat <saikat.halder@nav.no>
2021-08-11 16:45:44 +0200

    endret parameter navn fra originalBehandlingUuid

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/tilbakekreving/OpprettTilbakekrevingRevurderingRequest.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/tilbakekreving/OpprettTilbakekrevingRevurderingRequest.kt
@@ -2,5 +2,5 @@ package no.nav.familie.kontrakter.felles.tilbakekreving
 
 import java.util.UUID
 
-data class OpprettTilbakekrevingRevurderingRequest(val originalBehandlingUUID: UUID,
+data class OpprettTilbakekrevingRevurderingRequest(val originalBehandlingEksternBrukId: UUID,
                                                    val årsakstype:Behandlingsårsakstype)

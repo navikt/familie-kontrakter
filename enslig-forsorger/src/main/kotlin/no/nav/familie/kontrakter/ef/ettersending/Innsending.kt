b88367c5ce39a6766d6863252959117bf14cd9aa
Viktor <viktor.grondalen.solberg@nav.no>
2021-08-23 14:00:02 +0200

    Fikset import av dokument

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/ettersending/Innsending.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/ettersending/Innsending.kt
@@ -1,5 +1,7 @@
 package no.nav.familie.kontrakter.ef.ettersending
 
+import no.nav.familie.kontrakter.ef.søknad.Dokument
+
 data class Innsending(val beskrivelse: String,
                       val dokumenttype: String,
-                      val vedlegg: List<no.nav.familie.kontrakter.ef.søknad.Dokument>)
+                      val vedlegg: List<Dokument>)

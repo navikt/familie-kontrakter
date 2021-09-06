160c0cd8d26a9fd50b2116f616dff3b0e65dc8cb
ma10s <mattis.janitz@nav.no>
2021-09-06 12:23:14 +0200

    Brukte gammel/feil formateringsfil.

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/iverksett/BehandlingsstatistikkDto.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/iverksett/BehandlingsstatistikkDto.kt
@@ -6,22 +6,22 @@ import java.time.ZonedDateTime
 import java.util.UUID
 
 data class BehandlingsstatistikkDto(
-    val behandlingId: UUID,
-    val personIdent: String,
-    val gjeldendeSaksbehandlerId: String,
-    val eksternFagsakId: String,
-    val hendelseTidspunkt: ZonedDateTime,
-    @Deprecated("bruk henvendelseTidspunkt") val søknadstidspunkt: ZonedDateTime? = null,
-    val hendelse: Hendelse,
-    val behandlingResultat: String? = null,
-    val resultatBegrunnelse: String? = null,
-    val opprettetEnhet: String,
-    val ansvarligEnhet: String,
-    val strengtFortroligAdresse: Boolean,
-    val stønadstype: StønadType,
-    val behandlingstype: BehandlingType,
-    val henvendelseTidspunkt: ZonedDateTime? = null, // erstatter søknadstidspunkt
-    val relatertBehandlingId: UUID?
+        val behandlingId: UUID,
+        val personIdent: String,
+        val gjeldendeSaksbehandlerId: String,
+        val eksternFagsakId: String,
+        val hendelseTidspunkt: ZonedDateTime,
+        @Deprecated("bruk henvendelseTidspunkt") val søknadstidspunkt: ZonedDateTime? = null,
+        val hendelse: Hendelse,
+        val behandlingResultat: String? = null,
+        val resultatBegrunnelse: String? = null,
+        val opprettetEnhet: String,
+        val ansvarligEnhet: String,
+        val strengtFortroligAdresse: Boolean,
+        val stønadstype: StønadType,
+        val behandlingstype: BehandlingType,
+        val henvendelseTidspunkt: ZonedDateTime? = null, // erstatter søknadstidspunkt
+        val relatertBehandlingId: UUID?
 )
 
 enum class Hendelse {

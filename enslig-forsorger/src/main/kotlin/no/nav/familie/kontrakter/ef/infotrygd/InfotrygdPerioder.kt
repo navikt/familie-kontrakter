69f447bc797ccc3ee4275ba29a1aff0db04fab5f
Johan Blomgren <blommish@users.noreply.github.com>
2021-09-30 07:14:38 +0200

    Legger til nye request og responer for å hente data fra replikatjenesten. Renamer også tidligere arena-objekter DTOer

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/infotrygd/InfotrygdPerioder.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/infotrygd/InfotrygdPerioder.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.kontrakter.ef.infotrygd
 
+import no.nav.familie.kontrakter.ef.felles.StønadType
 import java.time.LocalDate
 
 /**
@@ -9,10 +10,29 @@ data class InfotrygdPerioderArenaRequest(val personIdenter: Set<String>,
                                          val fomDato: LocalDate? = null,
                                          val tomDato: LocalDate? = null)
 
-data class InfotrygdPerioderResponse(val perioder: List<InfotrygdPeriode>)
+data class InfotrygdPerioderArenaResponse(val perioder: List<InfotrygdArenaPeriode>)
+
+data class InfotrygdArenaPeriode(val personIdent: String,
+                                 val fomDato: LocalDate,
+                                 val tomDato: LocalDate,
+                                 val beløp: Float,
+                                 val opphørsdato: LocalDate? = null)
+
+data class InfotrygdPeriodeRequest(val personIdenter: Set<String>,
+                                   val stønadstyper: Set<StønadType>)
+
+data class InfotrygdPeriodeResponse(val perioder: List<InfotrygdPeriode>)
 
 data class InfotrygdPeriode(val personIdent: String,
-                            val fomDato: LocalDate,
-                            val tomDato: LocalDate,
-                            val beløp: Float,
-                            val opphørsdato: LocalDate? = null)
+                            val kode: String,
+                            val brukerId: String,
+                            val stønadId: Long,
+                            val vedtakId: Long,
+                            val stønadBeløp: Int,
+                            val inntektsreduksjon: Int,
+                            val samordningsfradrag: Int,
+                            val beløp: Int,
+                            val startDato: LocalDate,
+                            val stønadFom: LocalDate,
+                            val stønadTom: LocalDate,
+                            val opphørsdato: LocalDate?)

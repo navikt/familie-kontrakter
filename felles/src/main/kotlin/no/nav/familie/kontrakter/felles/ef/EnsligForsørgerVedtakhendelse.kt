53a6ef2b7a7e91d7acc0e24d02dfb4d420f72844
Johan Blomgren <blommish@users.noreply.github.com>
2021-10-05 10:12:12 +0200

    Lagt til data class for hendelse om vedtak fra enslig forsørger (#412)

---
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/ef/EnsligForsørgerVedtakhendelse.kt
@@ -0,0 +1,5 @@
+package no.nav.familie.kontrakter.felles.ef
+
+data class EnsligForsørgerVedtakhendelse(val behandlingId: Long,
+                                         val personIdent: String,
+                                         val stønadType: StønadType)

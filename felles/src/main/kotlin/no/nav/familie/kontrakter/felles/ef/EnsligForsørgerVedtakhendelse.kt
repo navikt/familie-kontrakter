2fd5b3d2444068c85f88a6b42ef0ac03e6b69fc5
Johan Blomgren <blommish@users.noreply.github.com>
2021-10-05 10:06:29 +0200

    Sletter fagsakId og vedtaksdato, så kan vi eventuellt legge til det senere

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/ef/EnsligForsørgerVedtakhendelse.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/ef/EnsligForsørgerVedtakhendelse.kt
@@ -1,9 +1,5 @@
 package no.nav.familie.kontrakter.felles.ef
 
-import java.time.LocalDate
-
-data class EnsligForsørgerVedtakhendelse(val fagsakId: Long,
-                                         val behandlingId: Long,
+data class EnsligForsørgerVedtakhendelse(val behandlingId: Long,
                                          val personIdent: String,
-                                         val vedtaksdato: LocalDate,
                                          val stønadType: StønadType)

211f92822a32a390a9ff5696f2ae6edcdd3b3f06
nav-jorn-ola-birkeland <47781778+nav-jorn-ola-birkeland@users.noreply.github.com>
2021-10-12 16:29:26 +0200

    Legger til kontraktstyper for beriket simuleringsresultat (#414)

---
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/simulering/BeriketSimuleringsresultat.kt
@@ -0,0 +1,5 @@
+package no.nav.familie.kontrakter.felles.simulering
+
+data class BeriketSimuleringsresultat(
+        val detaljer: DetaljertSimuleringResultat,
+        val oppsummering: Simuleringsoppsummering)

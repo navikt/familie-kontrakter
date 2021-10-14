1fecb96932da0661e77e59313de4cd0ab3a5fcfc
ma10s <mattis.janitz@nav.no>
2021-10-14 10:10:45 +0200

    Vil endre rekkefølge på skolepengerparameter. Rekkefølge brukes når vi lager pdf.

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/SøknadSkolepenger.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/SøknadSkolepenger.kt
@@ -1,12 +1,12 @@
 package no.nav.familie.kontrakter.ef.søknad
 
 data class SøknadSkolepenger(val personalia: Søknadsfelt<Personalia>,
-                             val barn: Søknadsfelt<List<Barn>>,
                              val innsendingsdetaljer: Søknadsfelt<Innsendingsdetaljer>,
                              val sivilstandsdetaljer: Søknadsfelt<Sivilstandsdetaljer>,
                              val medlemskapsdetaljer: Søknadsfelt<Medlemskapsdetaljer>,
                              val bosituasjon: Søknadsfelt<Bosituasjon>,
                              val sivilstandsplaner: Søknadsfelt<Sivilstandsplaner>? = null,
+                             val barn: Søknadsfelt<List<Barn>>,
                              val utdanning: Søknadsfelt<UnderUtdanning>,
                              val dokumentasjon: SkolepengerDokumentasjon)
 

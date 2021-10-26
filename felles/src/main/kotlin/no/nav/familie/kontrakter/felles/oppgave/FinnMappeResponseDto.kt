8605adf5465ea5e6787b99f7f93a0f2f444485dd
ma10s <mattis.janitz@nav.no>
2021-10-26 12:30:02 +0200

    Formatering.

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/oppgave/FinnMappeResponseDto.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/oppgave/FinnMappeResponseDto.kt
@@ -1,18 +1,18 @@
 package no.nav.familie.integrasjoner.client.rest
 
 data class FinnMappeResponseDto(
-    val antallTreffTotalt: Int,
-    val mapper: List<MappeDto>
+        val antallTreffTotalt: Int,
+        val mapper: List<MappeDto>
 )
 
 data class MappeDto(
-    val endretAv: String,
-    val endretTidspunkt: String,
-    val enhetsnr: String,
-    val id: Int,
-    val navn: String,
-    val opprettetAv: String,
-    val opprettetTidspunkt: String,
-    val tema: String,
-    val versjon: Int
+        val endretAv: String,
+        val endretTidspunkt: String,
+        val enhetsnr: String,
+        val id: Int,
+        val navn: String,
+        val opprettetAv: String,
+        val opprettetTidspunkt: String,
+        val tema: String,
+        val versjon: Int
 )

29bc22f6f4d4fb02df58959a017d51930e8d2bd4
ma10s <mattis.janitz@nav.no>
2021-10-26 12:25:28 +0200

    Vil legge til søk etter mappe i familie integrasjoner. Legger derfor til FinnMappeRequest og FinnMappeResponseDto i kontrakter.

---
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/oppgave/FinnMappeRequest.kt
@@ -0,0 +1,5 @@
+package no.nav.familie.integrasjoner.client.rest
+
+import no.nav.familie.kontrakter.felles.abstraction.QueryObject
+
+data class FinnMappeRequest (val tema : Array<String>, val enhetsnr: String, val opprettetFom: String?, val limit:Int = 1000) : QueryObject()

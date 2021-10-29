d469e6f53ad0017e78087e46bb8f0249d5c31810
Bent J. Lorentzen <bent.lorentzen@nav.no>
2021-10-29 15:08:58 +0200

    Filtrerer vekk tomme lister.

--- felles/src/test/kotlin/no/nav/familie/kontrakter/felles/abstraction/QueryUtilKtTest.kt
+++ felles/src/test/kotlin/no/nav/familie/kontrakter/felles/abstraction/QueryUtilKtTest.kt
@@ -3,15 +3,25 @@ package no.nav.familie.kontrakter.felles.abstraction
 import org.junit.Test
 import org.springframework.util.LinkedMultiValueMap
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 
 internal class QueryUtilKtTest {
 
     @Test
-    fun `toQueryParams gjør lister om til én string`() {
+    fun `toQueryParams ivaretar lister som lister`() {
         val queryParams: LinkedMultiValueMap<String, String> = toQueryParams(Testdata())
 
         assertEquals(listOf("1","2","3","4","5"), queryParams["list"])
     }
+
+    @Test
+    fun `toQueryParams filtrerer vekk tomme lister`() {
+        val queryParams: LinkedMultiValueMap<String, String> = toQueryParams(Testdata(list = listOf()))
+
+        assertFalse { queryParams.containsKey("list") }
+    }
+
+
 
     data class Testdata(val int: Int = 5,
                         val string: String = "Fem",

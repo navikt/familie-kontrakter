a601cb13f1bb2826f4c5cb2437175b08b8251d14
eirarset <eirik.arseth@nav.no>
2021-11-04 09:00:24 +0100

    Tittel på mangelbrev skaÃl være "Innhenitng av opplysninger"

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/felles/FrittståendeBrevDto.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/felles/FrittståendeBrevDto.kt
@@ -9,9 +9,9 @@ data class FrittståendeBrevDto(val personIdent: String,
                                val saksbehandlerIdent: String)
 
 enum class FrittståendeBrevType(val tittel: String) {
-    MANGELBREV_OVERGANGSSTØNAD("Mangelbrev overgangsstønad"),
-    MANGELBREV_BARNETILSYN("Mangelbrev barnetilsyn"),
-    MANGELBREV_SKOLEPENGER("Mangelbrev skolepenger"),
+    MANGELBREV_OVERGANGSSTØNAD("Innhenting av opplysninger"),
+    MANGELBREV_BARNETILSYN("Innhenting av opplysninger"),
+    MANGELBREV_SKOLEPENGER("Innhenting av opplysninger"),
     INFOBREV_OVERGANGSSTØNAD("Infobrev overgangsstønad"),
     INFOBREV_BARNETILSYN("Infobrev barnetilsyn"),
     INFOBREV_SKOLEPENGER("Infobrev skolepenger"),

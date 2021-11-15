800918dd2fbb8a68fd08415799c45245430296a4
Bent J. Lorentzen <bent.lorentzen@nav.no>
2021-11-15 15:57:11 +0100

    Fikset stønadskoder for EF.

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/tilbakekreving/Ytelsestype.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/tilbakekreving/Ytelsestype.kt
@@ -6,11 +6,11 @@ import no.nav.familie.kontrakter.felles.Tema
 enum class Ytelsestype(val kode: String, val navn: Map<Språkkode, String>) {
     BARNETRYGD("BA", mapOf(Språkkode.NB to "Barnetrygd",
                            Språkkode.NN to "Barnetrygd")),
-    OVERGANGSSTØNAD("OG", mapOf(Språkkode.NB to "Overgangsstønad",
+    OVERGANGSSTØNAD("EFOG", mapOf(Språkkode.NB to "Overgangsstønad",
                                 Språkkode.NN to "Overgangsstønad")),
-    BARNETILSYN("BT", mapOf(Språkkode.NB to "Stønad til barnetilsyn",
+    BARNETILSYN("EFBT", mapOf(Språkkode.NB to "Stønad til barnetilsyn",
                             Språkkode.NN to "Stønad til barnetilsyn")),
-    SKOLEPENGER("SP", mapOf(Språkkode.NB to "Stønad til skolepenger",
+    SKOLEPENGER("EFSP", mapOf(Språkkode.NB to "Stønad til skolepenger",
                             Språkkode.NN to "Stønad til skulepengar")),
     KONTANTSTØTTE("KS", mapOf(Språkkode.NB to "Kontantstøtte",
                               Språkkode.NN to "Kontantstøtte"));

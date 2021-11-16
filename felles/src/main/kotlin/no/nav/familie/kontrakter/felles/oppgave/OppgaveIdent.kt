66bbac2c4c51d82fe859b55c2a61b618d2a56588
Marcus Gustafson <marcus.gustafson@computas.com>
2021-11-16 12:30:15 +0100

    Deprecated AKTOERI

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/oppgave/OppgaveIdent.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/oppgave/OppgaveIdent.kt
@@ -4,6 +4,8 @@ data class OppgaveIdentV2(val ident: String?, val gruppe: IdentGruppe?)
 
 enum class IdentGruppe {
     AKTORID,
+    @Deprecated("Erstates av AKTOERID")
+    AKTOERID,
     FOLKEREGISTERIDENT,
     NPID,
     ORGNR,

d52f9c3ec01bc744c1879b6d505f59a12df4789f
Marcus Gustafson <marcus.gustafson@computas.com>
2022-01-17 14:01:24 +0100

    La til aktørId til personident

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/PersonIdent.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/PersonIdent.kt
@@ -2,4 +2,7 @@ package no.nav.familie.kontrakter.felles
 
 import javax.validation.constraints.Pattern
 
-data class PersonIdent(@field:Pattern(regexp = "(^$|.{11})", message = "PersonIdent er ikke riktig") val ident: String)
+data class PersonIdent(
+    @field:Pattern(regexp = "(^$|.{11})", message = "PersonIdent er ikke riktig") val ident: String,
+    @field:Pattern(regexp = "(^$|.{13})", message = "AktørId er ikke riktig") val aktørId: String? = null
+)

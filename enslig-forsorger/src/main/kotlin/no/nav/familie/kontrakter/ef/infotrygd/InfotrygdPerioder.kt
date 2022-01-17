1a232771d82807ac9838cf0620b728cd80a700c4
Johan Blomgren <blommish@users.noreply.github.com>
2022-01-17 11:12:40 +0100

    Legger til koder for aktivitet og periode i InfotrygdPerioder.kt (#466)
    * Legger til koder for aktivitet og periode i InfotrygdPerioder.kt

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/infotrygd/InfotrygdPerioder.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/infotrygd/InfotrygdPerioder.kt
@@ -26,9 +26,14 @@ data class InfotrygdPeriodeResponse(val overgangsstønad: List<InfotrygdPeriode>
                                     val barnetilsyn: List<InfotrygdPeriode>,
                                     val skolepenger: List<InfotrygdPeriode>)
 
+/**
+ * @param kodeOvergangsstønad er i prinsipp det vi kaller periodetype i ny løsning
+ */
 data class InfotrygdPeriode(val personIdent: String,
                             val sakstype: InfotrygdSakstype,
                             val kode: InfotrygdEndringKode,
+                            val kodeOvergangsstønad: InfotrygdOvergangsstønadKode?,
+                            val aktivitetstype: InfotrygdAktivitetstype?,
                             val brukerId: String,
                             val stønadId: Long,
                             val vedtakId: Long,
@@ -44,7 +49,7 @@ data class InfotrygdPeriode(val personIdent: String,
                             val opphørsdato: LocalDate?)
 
 @Suppress("unused")
-enum class InfotrygdEndringKode(val kode: String, val beskrivelse: String) {
+enum class InfotrygdEndringKode(val infotrygdKode: String, val beskrivelse: String) {
 
     ANNULERT("AN", "Annullert"),
     ENDRING_BEREGNINGSGRUNNLAG("E", "Endring i beregningsgrunnlag"),
@@ -54,10 +59,12 @@ enum class InfotrygdEndringKode(val kode: String, val beskrivelse: String) {
     OPPHØRT("O", "Opphørt"),
     SATSENDRING("S", "Satsendring"),
     UAKTUELL("UA", "Uaktuell"),
-    OVERTFØRT_NY_LØSNING("OO", "Overf ny løsning")
+    OVERTFØRT_NY_LØSNING("OO", "Overf ny løsning");
 }
 
-enum class InfotrygdSakstype(val kode: String, val beskrivelse: String) {
+@Suppress("unused")
+enum class InfotrygdSakstype(val infotrygdKode: String, val beskrivelse: String) {
+
     KLAGE("K", "Klage"),
     MASKINELL_G_OMREGNING("MG", "Maskinell G-omregning"),
     REVURDERING("R", "Revurdering"),
@@ -67,4 +74,31 @@ enum class InfotrygdSakstype(val kode: String, val beskrivelse: String) {
     ANKE("A", "Anke"),
     SØKNAD("S", "Søknad"),
     SØKNAD_ØKNING_ENDRING("SØ", "Søknad om økning/endring")
+}
+
+@Suppress("unused")
+enum class InfotrygdOvergangsstønadKode(val infotrygdKode: String, val beskrivelse: String) {
+
+    BARN_UNDER_1_3_ÅR("1", "Barn under 1 år / 3 år (gamle tilfeller)"),
+    YRKESRETTET_AKTIVITET_BARN_FYLT_1_3_ÅR("2", "Er i yrkesrettet aktivitet - barn har fylt 1 år / 3 år (gamle tilfeller)"),
+    UNNTAK_FRA_KRAV_TIL_YRKESRETTET_AKTIVITET("3",
+                                              "Unntak fra krav til yrkesr. aktivitet når barn har fylt 1 år / år (gamle tilfeller)"),
+    UTVIDELSE_NØDVENDIG_UTDANNING("4", "Utvidelse på grunn av nødvendig utdanning jf 15-6. 3. ledd"),
+    PÅVENTE_SKOLESTART_ARBEID_TILSYNSPLASS("5", "I påvente av skolestart/arbeid/tilsynsplass 15-6. 4. ledd"),
+    YRKESRETTET_AKTIVITET("6", "Er i yrkesrettet aktivitet - i omstillingstid"),
+    FORBIGÅENDE_SYKDOM("7", "Forbig. sykdom hos forsørger eller barnet 15-6. 6. ledd"),
+    SÆRLIG_TILSSYNSKREVENDE_BARN("8", "Har særlig tilssynskrevende barn"),
+    ETABLERER_EGEN_VIRKSOMHET("9", "Etablerer egen virksomhet"),
+    FORTSATT_INNVILGET_TROSS_VARSEL_OM_OPPHØR_PGA_SAMBOER("10", "Fortsatt innvilget tro");
+}
+
+@Suppress("unused")
+enum class InfotrygdAktivitetstype(val infotrygdKode: String, val beskrivelse: String) {
+
+    I_ARBEID("A", "I arbeid"),
+    I_UTDANNING("U", "I utdanning"),
+    TILMELDT_SOM_REELL_ARBEIDSSØKER("S", "Tilmeldt som reell arbeidssøker"),
+    KURS("K", "Kurs o.l."),
+    BRUKERKONTAKT("B", "Brukerkontakt"),
+    IKKE_I_AKTIVITET("N", "Ikke i aktivitet");
 }

4be71586b4f448eae8edbe1c9f6338ff119723e4
haldersaikat <saikat.halder@nav.no>
2022-04-06 09:24:18 +0200

    fjernet privat access modifier

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/simulering/HentFeilutbetalingerFraSimuleringRequest.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/simulering/HentFeilutbetalingerFraSimuleringRequest.kt
@@ -4,14 +4,14 @@ import no.nav.familie.kontrakter.felles.Fagsystem
 import java.math.BigDecimal
 import java.time.LocalDate
 
-data class HentFeilutbetalingerFraSimuleringRequest(private val fagsystem: Fagsystem,
-                                                    private val eksternFagsakId: String,
-                                                    private val behandlingId: String)
+data class HentFeilutbetalingerFraSimuleringRequest(val fagsystem: Fagsystem,
+                                                    val eksternFagsakId: String,
+                                                    val behandlingId: String)
 
-data class FeilutbetalingerFraSimulering(private val feilutbetaltePerioder: List<FeilutbetaltPeriode>)
+data class FeilutbetalingerFraSimulering(val feilutbetaltePerioder: List<FeilutbetaltPeriode>)
 
-data class FeilutbetaltPeriode(private val fom: LocalDate,
-                               private val tom: LocalDate,
-                               private val nyttBeløp: BigDecimal,
-                               private val tidligereUtbetaltBeløp: BigDecimal,
-                               private val feilutbetaltBeløp: BigDecimal)
+data class FeilutbetaltPeriode(val fom: LocalDate,
+                               val tom: LocalDate,
+                               val nyttBeløp: BigDecimal,
+                               val tidligereUtbetaltBeløp: BigDecimal,
+                               val feilutbetaltBeløp: BigDecimal)

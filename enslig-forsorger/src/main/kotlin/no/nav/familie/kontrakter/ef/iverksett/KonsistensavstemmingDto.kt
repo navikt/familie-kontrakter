6842a71b24f7f7edcc8e4acf6565fc599ab31451
Johan Blomgren <blommish@users.noreply.github.com>
2022-05-04 14:26:24 +0200

    Legger til avstemmingstidspunkt for konsistensavstemming sånn att alle meldinger om konsistensavstemming mot oppdrag blir lik (#535)

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/iverksett/KonsistensavstemmingDto.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/iverksett/KonsistensavstemmingDto.kt
@@ -1,11 +1,13 @@
 package no.nav.familie.kontrakter.ef.iverksett
 
 import no.nav.familie.kontrakter.felles.ef.StønadType
+import java.time.LocalDateTime
 import java.util.UUID
 
 data class KonsistensavstemmingDto(
         val stønadType: StønadType,
-        val tilkjenteYtelser: List<KonsistensavstemmingTilkjentYtelseDto>
+        val tilkjenteYtelser: List<KonsistensavstemmingTilkjentYtelseDto>,
+        val avstemmingstidspunkt: LocalDateTime? = null
 )
 
 data class KonsistensavstemmingTilkjentYtelseDto(

0ada3af9f3e82858b7bdccd08e930df4cec57bc8
Johan Blomgren <blommish@users.noreply.github.com>
2022-05-24 12:29:17 +0200

    Fjerner periodetype trengs ikke (#555)
    * Fjerner periodetype trengs ikke
    Type utbetaling gjøres i utbetalingsgeneratorn i ef-iverksett ut fra støndstype i stedet
    
    * Fjerner InntektDto då den ikke er i bruk

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/iverksett/AndeltilkjentYtelseDto.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/iverksett/AndeltilkjentYtelseDto.kt
@@ -5,7 +5,6 @@ import java.util.UUID
 
 data class AndelTilkjentYtelseDto(
         val beløp: Int,
-        var periodetype: Periodetype? = null, // TODO slett denne når den er ubrukt i ef-sak og iverksett, periodetype mappes i utbetalingsgeneratorn basert på stønadstype
         val inntekt: Int,
         val inntektsreduksjon: Int,
         val samordningsfradrag: Int,
@@ -13,8 +12,3 @@ data class AndelTilkjentYtelseDto(
         val tilOgMed: LocalDate,
         val kildeBehandlingId: UUID? = null
 )
-
-enum class Periodetype {
-    MÅNED,
-    ENGANGSUTBETALING,
-}

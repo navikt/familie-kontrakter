33e74cf557d7f712b52d01ebe7aefd76fceb21bf
Johan Blomgren <blommish@users.noreply.github.com>
2022-05-24 12:05:04 +0200

    Periodetype trengs ikke, setter den til optional for å unngå compile errors i koden, sletter i egen PR (#554)
    Type utbetaling gjøres i utbetalingsgeneratorn i ef-iverksett ut fra støndstype i stedet

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/iverksett/AndeltilkjentYtelseDto.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/iverksett/AndeltilkjentYtelseDto.kt
@@ -5,7 +5,7 @@ import java.util.UUID
 
 data class AndelTilkjentYtelseDto(
         val beløp: Int,
-        var periodetype: Periodetype,
+        var periodetype: Periodetype? = null, // TODO slett denne når den er ubrukt i ef-sak og iverksett, periodetype mappes i utbetalingsgeneratorn basert på stønadstype
         val inntekt: Int,
         val inntektsreduksjon: Int,
         val samordningsfradrag: Int,

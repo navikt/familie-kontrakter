dbc4330c9d33cdf7438af9a07577f2743f01567b
ma10s <mattis.janitz@nav.no>
2022-06-27 12:24:27 +0200

    Vil legge til flagg for selvstendig og tilsynskrevende så vi kan rute til riktig mappe i mottak.

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/SøknadMedVedlegg.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/SøknadMedVedlegg.kt
@@ -4,5 +4,7 @@ data class SøknadMedVedlegg<T>(
     val søknad: T,
     val vedlegg: List<Vedlegg>,
     val dokumentasjonsbehov: List<Dokumentasjonsbehov> = emptyList(),
-    val behandleINySaksbehandling: Boolean = false
+    val behandleINySaksbehandling: Boolean = false,
+    val erSelvstendigNæringsdrivende: Boolean = false,
+    val harSærligTilsynskrevendeBarn: Boolean = false
 )

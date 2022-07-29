0b158aa3375842e467c16c7427ec7fccf5a725da
bent-lorentzen <54444412+bent-lorentzen@users.noreply.github.com>
2022-07-29 13:59:57 +0200

    Felles periode (#589)
    * Ny felles periodeklasse som støtter både datoperioder og månedsperioder.

---
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/Periode.kt
@@ -0,0 +1,81 @@
+package no.nav.familie.kontrakter.felles
+
+import java.time.LocalDate
+import java.time.YearMonth
+
+data class Periode(
+    val fomDato: LocalDate,
+    val tomDato: LocalDate
+) : Comparable<Periode> {
+
+    val fomMåned = YearMonth.from(fomDato)
+    val tomMåned = YearMonth.from(tomDato)
+
+    constructor(
+        fomMåned: YearMonth,
+        tomMåned: YearMonth
+    ) : this(fomMåned.atDay(1), tomMåned.atEndOfMonth())
+
+    init {
+        require(tomDato >= fomDato) { "Til-og-med før fra-og-med: $fomDato > $tomDato" }
+    }
+
+    fun inneholder(dato: LocalDate): Boolean {
+        return dato in fomDato..tomDato
+    }
+
+    fun inneholder(måned: YearMonth): Boolean {
+        return inneholder(Periode(måned.atDay(1), måned.atEndOfMonth()))
+    }
+
+    fun inneholder(annen: Periode): Boolean {
+        return annen.fomDato >= this.fomDato && annen.tomDato <= this.tomDato
+    }
+
+    fun omsluttesAv(annen: Periode): Boolean {
+        return annen.fomDato <= fomDato && annen.tomDato >= tomDato
+    }
+
+    fun overlapper(other: Periode): Boolean {
+        return inneholder(other.fomDato) || inneholder(other.tomDato) || other.inneholder(fomDato)
+    }
+
+    fun snitt(annen: Periode): Periode? {
+        return if (!overlapper(annen)) {
+            null
+        } else if (this == annen) {
+            this
+        } else {
+            Periode(
+                maxOf(fomDato, annen.fomDato),
+                minOf(tomDato, annen.tomDato)
+            )
+        }
+    }
+
+    fun overlapperIStartenAv(annen: Periode) =
+        fomDato <= annen.fomDato &&
+            tomDato > annen.fomDato &&
+            tomDato < annen.tomDato
+
+    fun overlapperISluttenAv(annen: Periode) =
+        fomDato > annen.fomDato &&
+            fomDato < annen.tomDato &&
+            tomDato >= annen.tomDato
+
+    fun lengdeIHeleMåneder(): Long {
+        require(fomDato == fomMåned.atDay(1) && tomDato == tomMåned.atEndOfMonth()) {
+            "Forsøk på å beregne lengde i hele måneder for en periode som ikke er hele måneder: $fomDato - $tomDato"
+        }
+        return (tomMåned.year * 12 + tomMåned.monthValue) - (fomMåned.year * 12 + fomMåned.monthValue) + 1L
+    }
+
+    companion object {
+
+        val COMPARATOR: Comparator<Periode> = Comparator.comparing(Periode::fomDato).thenComparing(Periode::tomDato)
+    }
+
+    override fun compareTo(other: Periode): Int {
+        return COMPARATOR.compare(this, other)
+    }
+}

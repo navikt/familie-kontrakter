64219b5172b726f0d97b37179cffe63b804df09d
Bent J. Lorentzen <bent.lorentzen@nav.no>
2022-08-23 12:03:22 +0200

    Fikset deserialisering av månedsperiode.

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/ObjectMapper.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/ObjectMapper.kt
@@ -6,7 +6,10 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.datatype.jsr310.deser.YearMonthDeserializer
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import java.time.YearMonth
+import java.time.format.DateTimeFormatter
 
 val objectMapper: ObjectMapper
     get() = ObjectMapper()
@@ -15,7 +18,13 @@ val objectMapper: ObjectMapper
         .setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
         .setVisibility(PropertyAccessor.CREATOR, JsonAutoDetect.Visibility.ANY)
         .registerKotlinModule()
-        .registerModule(JavaTimeModule())
+        .registerModule(
+            JavaTimeModule()
+                .addDeserializer(
+                    YearMonth::class.java,
+                    YearMonthDeserializer(DateTimeFormatter.ofPattern("u-MM")) // Denne trengs for å parse år over 9999 riktig.
+                )
+        )
         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
         .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
         .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)

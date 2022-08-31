52c176f55402f8dfe75e0f243a05ebb98e9b4dd6
charliemidtlyng <charlie.midtlyng@bekk.no>
2022-08-31 22:09:21 +0200

    Stønadstype istedenfor StønadType

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/journalføring/AutomatiskJournalføringRequest.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/journalføring/AutomatiskJournalføringRequest.kt
@@ -5,5 +5,5 @@ import no.nav.familie.kontrakter.felles.ef.StønadType
 data class AutomatiskJournalføringRequest(
     val personIdent: String,
     val journalpostId: String,
-    val stønadType: StønadType
+    val stønadstype: StønadType
 )

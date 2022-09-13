42ac36f478d91320b9a5f75c205daca02cc6309f
Johan Blomgren <blommish@users.noreply.github.com>
2022-09-13 09:23:20 +0200

    småfiks

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/klage/Stønadstype.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/klage/Stønadstype.kt
@@ -10,21 +10,18 @@ enum class Stønadstype {
     SKOLEPENGER,
     KONTANTSTØTTE;
 
-    fun tilTema(): Tema {
-        return when (this) {
-            BARNETRYGD -> Tema.BAR
-            BARNETILSYN, OVERGANGSSTØNAD, SKOLEPENGER -> Tema.ENF
-            KONTANTSTØTTE -> Tema.KON
-        }
+    fun tilTema(): Tema = when (this) {
+        BARNETRYGD -> Tema.BAR
+        BARNETILSYN, OVERGANGSSTØNAD, SKOLEPENGER -> Tema.ENF
+        KONTANTSTØTTE -> Tema.KON
     }
 
     companion object {
-        fun fraEfStønadstype(stønadType: StønadType): Stønadstype {
-            return when(stønadType) {
-                StønadType.OVERGANGSSTØNAD -> OVERGANGSSTØNAD
-                StønadType.BARNETILSYN -> BARNETILSYN
-                StønadType.SKOLEPENGER -> SKOLEPENGER
-            }
+
+        fun fraEfStønadstype(stønadType: StønadType): Stønadstype = when (stønadType) {
+            StønadType.OVERGANGSSTØNAD -> OVERGANGSSTØNAD
+            StønadType.BARNETILSYN -> BARNETILSYN
+            StønadType.SKOLEPENGER -> SKOLEPENGER
         }
     }
 }

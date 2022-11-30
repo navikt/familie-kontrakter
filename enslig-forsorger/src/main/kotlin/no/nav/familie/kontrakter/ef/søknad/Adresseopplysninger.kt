02e6f1d1873d935ee20e1a8fcaedbcd06b91345f
Johan Blomgren <blommish@users.noreply.github.com>
2022-11-30 09:42:04 +0100

    Endrer navn på opplysningerOmAdresse til Adresseopplysninger som er bedre norsk. Den er ennå ikke i bruk

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Adresseopplysninger.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Adresseopplysninger.kt
@@ -1,6 +1,6 @@
 package no.nav.familie.kontrakter.ef.søknad
 
-data class OpplysningerOmAdresse(
+data class Adresseopplysninger(
     val søkerBorPåRegistrertAdresse: Søknadsfelt<Boolean>? = null,
     val harMeldtFlytteendring: Søknadsfelt<Boolean>? = null,
     val dokumentasjonFlytteendring: Søknadsfelt<Dokumentasjon>?

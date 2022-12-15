69771fe5538f119c617f031d9b9959a49fb66b70
Viktor <viktor.grondalen.solberg@nav.no>
2022-12-15 13:05:05 +0100

    skal kunne diffe fagsystemvedtak på om de er sanksjon, ordinær eller tilbakekreving

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/klage/FagsystemVedtak.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/klage/FagsystemVedtak.kt
@@ -7,10 +7,18 @@ data class FagsystemVedtak(
     val behandlingstype: String,
     val resultat: String,
     val vedtakstidspunkt: LocalDateTime,
-    val fagsystemType: FagsystemType
+    val fagsystemType: FagsystemType,
+    val vedtakType: VedtakType
 )
 
+@Deprecated("Bruk VedtakType")
 enum class FagsystemType {
     ORDNIÆR, // brukes for behandlinger fra ef-sak/ba-sak
     TILBAKEKREVING
+}
+
+enum class VedtakType {
+    ORDINÆR, // brukes for behandlinger fra ef-sak/ba-sak
+    TILBAKEKREVING,
+    SANKSJON_1_MND
 }

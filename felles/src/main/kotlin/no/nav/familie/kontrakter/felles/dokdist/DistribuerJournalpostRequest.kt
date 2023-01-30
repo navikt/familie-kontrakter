822a54d46d57bb9d7c272ec8e35703cbd8b62f89
haldersaikat <saikat.halder@nav.no>
2023-01-30 12:50:16 +0100

    Endret distribuer journalpost kontrakt for å inkludere manuell adresse

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/dokdist/DistribuerJournalpostRequest.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/dokdist/DistribuerJournalpostRequest.kt
@@ -9,6 +9,7 @@ data class DistribuerJournalpostRequest(
     @field:NotBlank val dokumentProdApp: String,
     val distribusjonstype: Distribusjonstype?,
     val distribusjonstidspunkt: Distribusjonstidspunkt = Distribusjonstidspunkt.KJERNETID,
+    val adresse: ManuellAdresse? = null
 )
 
 enum class Distribusjonstype {
@@ -20,4 +21,31 @@ enum class Distribusjonstype {
 enum class Distribusjonstidspunkt {
     KJERNETID,
     UMIDDELBART
+}
+
+enum class AdresseType {
+    norskPostadresse,
+    utenlandskPostadresse
+}
+
+data class ManuellAdresse(
+    val adresseType: AdresseType,
+    val adresseLinje1: String?,
+    val adresseLinje2: String?,
+    val adresseLinje3: String?,
+    val postnummer: String?,
+    val poststed: String?,
+    val land: String = "NO"
+) {
+    init {
+        check(land.length != 2) { "Ugyldig landkode" }
+        if (land == "NO") {
+            check(adresseType != AdresseType.norskPostadresse, { "Feil adresse type" })
+        }
+        if (adresseType == AdresseType.norskPostadresse) {
+            checkNotNull(adresseLinje1, { "AdresseLinje1 er påkrevd for norsk postadresse" })
+            checkNotNull(postnummer, { "Postnummer er påkrevd for norsk postadresse" })
+            checkNotNull(poststed, { "Poststed er påkrevd for norsk postadresse" })
+        }
+    }
 }

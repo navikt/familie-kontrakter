5cb3972d4f53eceff5ae5a16a40e47816c27c0ae
charliemidtlyng <charlie.midtlyng@bekk.no>
2023-03-16 10:21:58 +0100

    Skal tilby mer generiske navn Ãpå domeneklassene fra kontrakter

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/ef/PeriodeOvergangsstønad.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/ef/PeriodeOvergangsstønad.kt
@@ -3,26 +3,33 @@ package no.nav.familie.kontrakter.felles.ef
 import java.time.LocalDate
 
 /**
- * Klasser som brukes for å hente perioder for overgangsstønad fra enslig forsørger (ef-sak)
+ * Klasser som brukes for å hente perioder fra enslig forsørger (ef-sak)
  */
 
-data class PerioderOvergangsstønadRequest(
+data class EksternePerioderRequest(
     val personIdent: String,
     val fomDato: LocalDate? = null,
-    val tomDato: LocalDate? = null
+    val tomDato: LocalDate? = null,
 )
 
-data class PerioderOvergangsstønadResponse(val perioder: List<PeriodeOvergangsstønad>)
+data class EksternePerioderResponse(val perioder: List<EksternPeriode>)
+data class EksternePerioderMedBeløpResponse(val perioder: List<EksternPeriodeMedBeløp>)
 
-data class PeriodeOvergangsstønad(
+data class EksternPeriode(
     val personIdent: String,
     val fomDato: LocalDate,
     val tomDato: LocalDate,
-    val datakilde: Datakilde
-) {
+    val datakilde: Datakilde,
+)
 
-    enum class Datakilde {
-        INFOTRYGD,
-        EF
-    }
+data class EksternPeriodeMedBeløp(
+    val personIdent: String,
+    val fomDato: LocalDate,
+    val tomDato: LocalDate,
+    val datakilde: Datakilde,
+)
+
+enum class Datakilde {
+    INFOTRYGD,
+    EF,
 }

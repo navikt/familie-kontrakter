5cdbf0a5cc0e33c950f18a50a2d04b6dc629aeb6
sarahjelle <sara.hjelle@bekk.no>
2023-04-25 15:15:53 +0200

    Land i Utenlandsopphold må være nullable

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Utenlandsopphold.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Utenlandsopphold.kt
@@ -5,6 +5,6 @@ import java.time.LocalDate
 data class Utenlandsopphold(
     val fradato: Søknadsfelt<LocalDate>,
     val tildato: Søknadsfelt<LocalDate>,
-    val land: Søknadsfelt<String>,
+    val land: Søknadsfelt<String>? = null,
     val årsakUtenlandsopphold: Søknadsfelt<String>
 )

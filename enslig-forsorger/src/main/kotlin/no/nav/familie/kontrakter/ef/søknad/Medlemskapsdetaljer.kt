9402f3594c912dfbc04f5ca0ef10b38f27fb9287
Sara Hjelle <46678893+sarahjelle@users.noreply.github.com>
2023-04-25 15:28:28 +0200

    Skal ha med informasjon om nåværende oppholdsland og land for utenlan… (#774)
    * Skal ha med informasjon om nåværende oppholdsland og land for utenlandsperioder i medlemskapsdetaljer
    
    * Land i Utenlandsopphold må være nullable
    
    * TestsøknadBuilder må oppdateres med nye felter

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Medlemskapsdetaljer.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Medlemskapsdetaljer.kt
@@ -2,6 +2,7 @@ package no.nav.familie.kontrakter.ef.søknad
 
 data class Medlemskapsdetaljer(
     val oppholderDuDegINorge: Søknadsfelt<Boolean>,
+    val oppholdsland: Søknadsfelt<String>? = null,
     val bosattNorgeSisteÅrene: Søknadsfelt<Boolean>,
     val utenlandsopphold: Søknadsfelt<List<Utenlandsopphold>>? = null
 )

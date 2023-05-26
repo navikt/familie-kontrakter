4787da70b8c839c17bc46cb61c504fce75524c2f
Viktor Solberg <viktor.grondalen.solberg@nav.no>
2023-05-26 09:10:04 +0200

    Skal automatisk kunne utsende en ny type frittstående brev for innhenting av karakterutskrift (#790)

---
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/felles/KarakterutskriftBrevDto.kt
@@ -0,0 +1,15 @@
+package no.nav.familie.kontrakter.ef.felles
+
+import no.nav.familie.kontrakter.felles.ef.StønadType
+import java.time.Year
+
+data class KarakterutskriftBrevDto(
+        val fil: ByteArray,
+        val oppgaveId: Long,
+        val personIdent: String,
+        val eksternFagsakId: Long,
+        val journalførendeEnhet: String,
+        val brevtype: FrittståendeBrevType,
+        val gjeldendeÅr: Year,
+        val stønadType: StønadType,
+)

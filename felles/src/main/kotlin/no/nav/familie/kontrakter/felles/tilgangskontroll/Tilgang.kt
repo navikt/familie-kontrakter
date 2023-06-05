7ca65a5b48bbb59a20b90c75a7b29b9ce952adf9
Johan Blomgren <blommish@users.noreply.github.com>
2023-06-05 14:04:00 +0200

    Legger til personIdent i tilgang for å kunne sende flere identer fra BA og kunne vite hvilken som feilet (#795)

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/tilgangskontroll/Tilgang.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/tilgangskontroll/Tilgang.kt
@@ -1,6 +1,12 @@
 package no.nav.familie.kontrakter.felles.tilgangskontroll
 
+/**
+ * @param personIdent er identen på den personen man har sjekket tilgang på
+ * I de tilfeller der man sender en ident for å sjekker tilgang på personMedRelasjoner,
+ * så returnerer man fortsatt bare identen til den person som man etterspurt
+ */
 data class Tilgang(
     val harTilgang: Boolean,
     val begrunnelse: String? = null,
+    val personIdent: String? = null,
 )

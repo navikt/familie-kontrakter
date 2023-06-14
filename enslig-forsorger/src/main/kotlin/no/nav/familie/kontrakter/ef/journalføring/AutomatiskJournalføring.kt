b6056f3438a1fba823e8bbd834dad5cc7f6f3858
throndi <56258085+throndi@users.noreply.github.com>
2023-06-14 10:51:35 +0200

    Trenger et prioritet felt i AutomatiskJournalføringRequest mellom mottak og ef-sak siden mottak utleder prioritet

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/journalføring/AutomatiskJournalføring.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/journalføring/AutomatiskJournalføring.kt
@@ -1,6 +1,7 @@
 package no.nav.familie.kontrakter.ef.journalføring
 
 import no.nav.familie.kontrakter.felles.ef.StønadType
+import no.nav.familie.kontrakter.felles.oppgave.OppgavePrioritet
 import java.util.UUID
 
 data class AutomatiskJournalføringRequest(
@@ -8,6 +9,7 @@ data class AutomatiskJournalføringRequest(
     val journalpostId: String,
     val stønadstype: StønadType,
     val mappeId: Long?,
+    val prioritet: OppgavePrioritet = OppgavePrioritet.NORM,
 )
 
 data class AutomatiskJournalføringResponse(

3e35093288c7a497395162d5d2ac98cf820abdb9
Viktor <viktor.grondalen.solberg@nav.no>
2023-06-20 12:24:19 +0200

    mottak skal kunne spørre ef-sak om innsendt søknad har resultert i en behandle sak oppgave som er påbegynt av saksbehandler.

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/felles/KarakterutskriftBrevDto.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/felles/KarakterutskriftBrevDto.kt
@@ -4,12 +4,12 @@ import no.nav.familie.kontrakter.felles.ef.StønadType
 import java.time.Year
 
 data class KarakterutskriftBrevDto(
-        val fil: ByteArray,
-        val oppgaveId: Long,
-        val personIdent: String,
-        val eksternFagsakId: Long,
-        val journalførendeEnhet: String,
-        val brevtype: FrittståendeBrevType,
-        val gjeldendeÅr: Year,
-        val stønadType: StønadType,
+    val fil: ByteArray,
+    val oppgaveId: Long,
+    val personIdent: String,
+    val eksternFagsakId: Long,
+    val journalførendeEnhet: String,
+    val brevtype: FrittståendeBrevType,
+    val gjeldendeÅr: Year,
+    val stønadType: StønadType,
 )

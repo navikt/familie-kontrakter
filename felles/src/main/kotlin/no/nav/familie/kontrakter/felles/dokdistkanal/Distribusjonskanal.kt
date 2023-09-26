f862adbd3829e4df003fa90493c80f983c69c9bb
Tor Nærland <tor.nerland@nav.no>
2023-09-26 16:09:09 +0200

    Kontrakt for Dokdistkanal API

---
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/dokdistkanal/Distribusjonskanal.kt
@@ -0,0 +1,12 @@
+package no.nav.familie.kontrakter.felles.dokdistkanal
+
+enum class Distribusjonskanal {
+    PRINT,
+    SDP,
+    DITT_NAV,
+    LOKAL_PRINT,
+    INGEN_DISTRIBUSJON,
+    TRYGDERETTEN,
+    DPVT,
+    UKJENT,
+}

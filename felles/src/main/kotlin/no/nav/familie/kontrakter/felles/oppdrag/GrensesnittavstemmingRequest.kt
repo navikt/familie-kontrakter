5591a29233adf0c2bb00e719cb4880ba5dc71633
halvorbmundal <halvorbmundal@users.noreply.github.com>
2024-01-22 11:02:13 +0100

    Legg til avstemmingId (#868)
    * Legg til id
    
    * Fiks skrivefeil

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/oppdrag/GrensesnittavstemmingRequest.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/oppdrag/GrensesnittavstemmingRequest.kt
@@ -1,9 +1,15 @@
 package no.nav.familie.kontrakter.felles.oppdrag
 
 import java.time.LocalDateTime
+import java.util.UUID
 
+/**
+ * @property avstemmingId Unik id for en grenseavsnittsavstemming.
+ * Dersom det er fullført en grenseavsnittsavstemming på en avstemmingId vil alle fremdtidige grenseavsnittsavstemminger på samme avstemmingId ignoreres.
+ */
 data class GrensesnittavstemmingRequest(
     val fagsystem: String,
     val fra: LocalDateTime,
     val til: LocalDateTime,
+    val avstemmingId: UUID?,
 )

ee10001ae9496672d39fc6210c99963701aea79d
bragejahren <brage.ekroll.jahren@nav.no>
2024-08-21 15:46:39 +0200

    Legger til finnesPersonMedAdressebeskyttelse

--- kontantstotte/src/main/kotlin/no/nav/familie/kontrakter/ks/søknad/v5/KontantstøtteSøknad.kt
+++ kontantstotte/src/main/kotlin/no/nav/familie/kontrakter/ks/søknad/v5/KontantstøtteSøknad.kt
@@ -1,2 +1,27 @@
 package no.nav.familie.kontrakter.ks.søknad.v5
 
+import no.nav.familie.kontrakter.ks.søknad.v1.Locale
+import no.nav.familie.kontrakter.ks.søknad.v1.Søknaddokumentasjon
+import no.nav.familie.kontrakter.ks.søknad.v1.Søknadsfelt
+import no.nav.familie.kontrakter.ks.søknad.v1.TekstPåSpråkMap
+import no.nav.familie.kontrakter.ks.søknad.v4.Barn
+import no.nav.familie.kontrakter.ks.søknad.v4.Søker
+
+data class KontantstøtteSøknad(
+    val kontraktVersjon: Int,
+    val antallEøsSteg: Int,
+    val søker: Søker,
+    val barn: List<Barn>,
+    val dokumentasjon: List<Søknaddokumentasjon>,
+    val teksterTilPdf: Map<String, TekstPåSpråkMap>,
+    val originalSpråk: Locale,
+    val finnesPersonMedAdressebeskyttelse: Boolean,
+    val erNoenAvBarnaFosterbarn: Søknadsfelt<String>,
+    val søktAsylForBarn: Søknadsfelt<String>,
+    val oppholderBarnSegIInstitusjon: Søknadsfelt<String>,
+    val barnOppholdtSegTolvMndSammenhengendeINorge: Søknadsfelt<String>,
+    val erBarnAdoptert: Søknadsfelt<String>,
+    val mottarKontantstøtteForBarnFraAnnetEøsland: Søknadsfelt<String>,
+    val harEllerTildeltBarnehageplass: Søknadsfelt<String>,
+    val erAvdødPartnerForelder: Søknadsfelt<String>?,
+)

b8e4406541de849f744b391c247f6e1f1f43a811
bragejahren <brage.ekroll.jahren@nav.no>
2024-09-13 13:46:18 +0200

    Fjernet EnhetTilgang og skrevet tester for Enhet

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/enhet/Enhet.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/enhet/Enhet.kt
@@ -1,7 +1,5 @@
 package no.nav.familie.kontrakter.felles.enhet
 
-data class EnhetTilgang(val enheter: List<Enhet>)
-
 data class Enhet(val enhetsnummer: String) {
     init {
         if (enhetsnummer.length != 4) throw IllegalArgumentException("Enhetsnummer må være 4 siffer")

365d2c233b1ee04762b9d160647234b9dd889501
bragejahren <brage.ekroll.jahren@nav.no>
2024-09-17 15:34:02 +0200

    Lagt til enhetsnavn i kontrakt for Enhet

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/enhet/Enhet.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/enhet/Enhet.kt
@@ -1,6 +1,6 @@
 package no.nav.familie.kontrakter.felles.enhet
 
-data class Enhet(val enhetsnummer: String) {
+data class Enhet(val enhetsnummer: String, val enhetsnavn: String) {
     init {
         if (enhetsnummer.length != 4) throw IllegalArgumentException("Enhetsnummer må være 4 siffer")
     }

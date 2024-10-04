3350a8b12d8488645d73d21c666f204b781faaa4
bragejahren <70642183+bragejahren@users.noreply.github.com>
2024-10-04 10:20:17 +0200

    NAV-22361: Definerer Brevkoder for barnetrygd og kontantstøtte og utvider Journalpost med hjelpemetoder (#946)
    * Definerer brevkoder for barnetrygd og kontantstøtte og legger til hjelpemetoder i Journalpost
    
    * Lagt til enhetstester for Journalpost

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/journalpost/Journalpost.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/journalpost/Journalpost.kt
@@ -1,5 +1,8 @@
 package no.nav.familie.kontrakter.felles.journalpost
 
+import no.nav.familie.kontrakter.felles.Brevkoder
+import no.nav.familie.kontrakter.felles.Tema
+
 data class Journalpost(
     val journalpostId: String,
     val journalposttype: Journalposttype,
@@ -19,4 +22,17 @@ data class Journalpost(
 ) {
 
     val datoMottatt = relevanteDatoer?.firstOrNull { it.datotype == "DATO_REGISTRERT" }?.dato
+
+    fun erDigitalKanal() = this.kanal == "NAV_NO"
+
+    fun erDigitalBarnetrygdSøknad() = erDigitalKanal() && this.dokumenter?.any { dokument -> Brevkoder.BARNETRYGD_BREVKODER.any { brevkode -> brevkode == dokument.brevkode } } ?: false
+
+    fun erDigitalKontantstøtteSøknad() = erDigitalKanal() && this.dokumenter?.any { dokument -> Brevkoder.KONTANTSTØTTE_SØKNAD == dokument.brevkode } ?: false
+
+    fun erDigitalSøknad(tema: Tema): Boolean = when(tema) {
+        Tema.BAR -> erDigitalBarnetrygdSøknad()
+        Tema.KON -> erDigitalKontantstøtteSøknad()
+        else -> throw Error("Støtter ikke tema $tema")
+    }
+
 }
---
+++ felles/src/test/kotlin/no/nav/familie/kontrakter/felles/journalpost/JournalpostTest.kt
@@ -0,0 +1,192 @@
+package no.nav.familie.kontrakter.felles.journalpost
+
+import no.nav.familie.kontrakter.felles.Brevkoder
+import no.nav.familie.kontrakter.felles.Tema
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
+
+class JournalpostTest {
+
+    @Test
+    fun `skal returnere true dersom journalpost er digital kontantstøtte søknad og tema er KON`() {
+        //Arrange
+        val journalpost = Journalpost(
+            journalpostId = "123",
+            journalposttype = Journalposttype.I,
+            journalstatus = Journalstatus.MOTTATT,
+            kanal = "NAV_NO",
+            dokumenter = listOf(
+                DokumentInfo(
+                    dokumentInfoId = "1",
+                    brevkode = Brevkoder.KONTANTSTØTTE_SØKNAD
+                )
+            )
+        )
+
+        // Act
+        val erDigitalSøknad = journalpost.erDigitalSøknad(Tema.KON)
+
+        // Assert
+        assertTrue(erDigitalSøknad)
+    }
+
+    @Test
+    fun `skal returnere false dersom journalpost ikke er digital kontantstøtte søknad og tema er KON`() {
+        //Arrange
+        val journalpost = Journalpost(
+            journalpostId = "123",
+            journalposttype = Journalposttype.I,
+            journalstatus = Journalstatus.MOTTATT,
+            kanal = "NAV_NO",
+            dokumenter = listOf(
+                DokumentInfo(
+                    dokumentInfoId = "1",
+                    brevkode = Brevkoder.BARNETRYGD_ORDINÆR_SØKNAD
+                )
+            )
+        )
+
+        // Act
+        val erDigitalSøknad = journalpost.erDigitalSøknad(Tema.KON)
+
+        // Assert
+        assertFalse(erDigitalSøknad)
+    }
+
+    @Test
+    fun `skal returnere false dersom journalpost ikke inneholder noen dokumenter og tema er KON`() {
+        //Arrange
+        val journalpost = Journalpost(
+            journalpostId = "123",
+            journalposttype = Journalposttype.I,
+            journalstatus = Journalstatus.MOTTATT,
+            kanal = "NAV_NO",
+            dokumenter = null
+        )
+
+        // Act
+        val erDigitalSøknad = journalpost.erDigitalSøknad(Tema.KON)
+
+        // Assert
+        assertFalse(erDigitalSøknad)
+    }
+
+    @Test
+    fun `skal returnere false dersom journalpost ikke har kommet via digital kanal og tema er KON`() {
+        //Arrange
+        val journalpost = Journalpost(
+            journalpostId = "123",
+            journalposttype = Journalposttype.I,
+            journalstatus = Journalstatus.MOTTATT,
+            kanal = "SKAN_",
+            dokumenter = emptyList()
+        )
+
+        // Act
+        val erDigitalSøknad = journalpost.erDigitalSøknad(Tema.KON)
+
+        // Assert
+        assertFalse(erDigitalSøknad)
+    }
+
+    @Test
+    fun `skal returnere true dersom journalpost er digital barnetrygd søknad og tema er BAR`() {
+        //Arrange
+        val journalpost = Journalpost(
+            journalpostId = "123",
+            journalposttype = Journalposttype.I,
+            journalstatus = Journalstatus.MOTTATT,
+            kanal = "NAV_NO",
+            dokumenter = listOf(
+                DokumentInfo(
+                    dokumentInfoId = "1",
+                    brevkode = Brevkoder.BARNETRYGD_ORDINÆR_SØKNAD
+                )
+            )
+        )
+
+        // Act
+        val erDigitalSøknad = journalpost.erDigitalSøknad(Tema.BAR)
+
+        // Assert
+        assertTrue(erDigitalSøknad)
+    }
+
+    @Test
+    fun `skal returnere false dersom journalpost ikke er digital barnetrygd søknad og tema er BAR`() {
+        //Arrange
+        val journalpost = Journalpost(
+            journalpostId = "123",
+            journalposttype = Journalposttype.I,
+            journalstatus = Journalstatus.MOTTATT,
+            kanal = "NAV_NO",
+            dokumenter = listOf(
+                DokumentInfo(
+                    dokumentInfoId = "1",
+                    brevkode = Brevkoder.KONTANTSTØTTE_SØKNAD
+                )
+            )
+        )
+
+        // Act
+        val erDigitalSøknad = journalpost.erDigitalSøknad(Tema.BAR)
+
+        // Assert
+        assertFalse(erDigitalSøknad)
+    }
+
+    @Test
+    fun `skal returnere false dersom journalpost ikke inneholder noen dokumenter og tema er BAR`() {
+        //Arrange
+        val journalpost = Journalpost(
+            journalpostId = "123",
+            journalposttype = Journalposttype.I,
+            journalstatus = Journalstatus.MOTTATT,
+            kanal = "NAV_NO",
+            dokumenter = null
+        )
+
+        // Act
+        val erDigitalSøknad = journalpost.erDigitalSøknad(Tema.BAR)
+
+        // Assert
+        assertFalse(erDigitalSøknad)
+    }
+
+    @Test
+    fun `skal returnere false dersom journalpost ikke har kommet via digital kanal og tema er BAR`() {
+        //Arrange
+        val journalpost = Journalpost(
+            journalpostId = "123",
+            journalposttype = Journalposttype.I,
+            journalstatus = Journalstatus.MOTTATT,
+            kanal = "SKAN_",
+            dokumenter = emptyList()
+        )
+
+        // Act
+        val erDigitalSøknad = journalpost.erDigitalSøknad(Tema.BAR)
+
+        // Assert
+        assertFalse(erDigitalSøknad)
+    }
+
+    @Test
+    fun `skal kaste feil dersom tema enda ikke er støttet`() {
+        // Arrange
+        val journalpost = Journalpost(
+            journalpostId = "123",
+            journalposttype = Journalposttype.I,
+            journalstatus = Journalstatus.MOTTATT
+        )
+
+        // Act
+        val error = assertThrows<Error> { journalpost.erDigitalSøknad(Tema.ENF) }
+
+        // Assert
+        assertEquals(expected = "Støtter ikke tema ${Tema.ENF}", actual = error.message)
+    }
+}

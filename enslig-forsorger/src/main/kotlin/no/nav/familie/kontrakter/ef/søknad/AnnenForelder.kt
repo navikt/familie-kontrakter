d85d965cfeebfa8eb4450f372237cb885a77b8c1
RobinLundSadun <robin.lund.sadun@gmail.com>
2024-12-05 12:51:20 +0100

    Flyttet bosattNorge under personalia

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/AnnenForelder.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/AnnenForelder.kt
@@ -2,7 +2,7 @@ package no.nav.familie.kontrakter.ef.søknad
 
 data class AnnenForelder(
     val ikkeOppgittAnnenForelderBegrunnelse: Søknadsfelt<String>? = null,
+    val person: Søknadsfelt<PersonMinimum>? = null,
     val bosattNorge: Søknadsfelt<Boolean>? = null,
     val land: Søknadsfelt<String>? = null,
-    val person: Søknadsfelt<PersonMinimum>? = null,
 )

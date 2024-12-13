4eb57a25b9b9c9845f15f9d20dc37361691973db
RobinLundSadun <robin.lund.sadun@gmail.com>
2024-12-13 08:53:29 +0100

    søknadFraBestemtMåned over evt måned og år

--- enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Stønadsstart.kt
+++ enslig-forsorger/src/main/kotlin/no/nav/familie/kontrakter/ef/søknad/Stønadsstart.kt
@@ -3,7 +3,7 @@ package no.nav.familie.kontrakter.ef.søknad
 import java.time.Month
 
 data class Stønadsstart(
+    val søkerFraBestemtMåned: Søknadsfelt<Boolean>,
     val fraMåned: Søknadsfelt<Month>?,
     val fraÅr: Søknadsfelt<Int>?,
-    val søkerFraBestemtMåned: Søknadsfelt<Boolean>,
 )

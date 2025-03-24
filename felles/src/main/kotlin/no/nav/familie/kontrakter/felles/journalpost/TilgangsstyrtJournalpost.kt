c04b578fdabe9eac56cef412731f547e95e8b07e
bragejahren <70642183+bragejahren@users.noreply.github.com>
2025-03-24 14:31:09 +0100

    Justerer TilgangsstyrtJournalpost ved å erstatte harTilgang med data class JournalpostTilgang (#1004)

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/journalpost/TilgangsstyrtJournalpost.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/journalpost/TilgangsstyrtJournalpost.kt
@@ -2,5 +2,10 @@ package no.nav.familie.kontrakter.felles.journalpost
 
 data class TilgangsstyrtJournalpost(
     val journalpost: Journalpost,
+    val journalpostTilgang: JournalpostTilgang,
+)
+
+data class JournalpostTilgang(
     val harTilgang: Boolean,
+    val begrunnelse: String? = null,
 )

b55d05b1b8c2ab653c9e8c735ad5d69d80624508
Stig Strøm <stig.helge.strom@nav.no>
2025-08-19 18:29:59 +0200

    Utvidelse av organisjasjonskontrakt for å få med adresse. I første versjon kun kommunenummer (#1042)

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/organisasjon/Organisasjon.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/organisasjon/Organisasjon.kt
@@ -3,4 +3,11 @@ package no.nav.familie.kontrakter.felles.organisasjon
 data class Organisasjon(
     val organisasjonsnummer: String,
     val navn: String,
+    val adresse: OrganisasjonAdresse? = null,
+)
+
+// https://github.com/navikt/ereg-services/blob/main/src/main/java/no/nav/ereg/provider/rs/organisasjon/contract/Adresse.java
+data class OrganisasjonAdresse(
+    val type: String,
+    val kommunenummer: String?,
 )

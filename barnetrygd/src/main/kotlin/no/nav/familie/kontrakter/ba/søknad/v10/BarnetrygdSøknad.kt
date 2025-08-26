ee971117a32176a494b370b36f5919b99464c936
bragejahren <70642183+bragejahren@users.noreply.github.com>
2025-08-26 12:19:39 +0200

    Legger til svalbardOppholdPerioder på Barn og utvider Dokumentasjonsbehov med OPPHOLD_SVALBARD (#1047)

--- barnetrygd/src/main/kotlin/no/nav/familie/kontrakter/ba/søknad/v10/BarnetrygdSøknad.kt
+++ barnetrygd/src/main/kotlin/no/nav/familie/kontrakter/ba/søknad/v10/BarnetrygdSøknad.kt
@@ -10,15 +10,19 @@ import no.nav.familie.kontrakter.ba.søknad.v4.Søknadsfelt
 import no.nav.familie.kontrakter.ba.søknad.v4.Søknadstype
 import no.nav.familie.kontrakter.ba.søknad.v4.TidligereSamboer
 import no.nav.familie.kontrakter.ba.søknad.v4.Utenlandsopphold
+import no.nav.familie.kontrakter.ba.søknad.v5.RegistrertBostedType
 import no.nav.familie.kontrakter.ba.søknad.v7.IdNummer
 import no.nav.familie.kontrakter.ba.søknad.v7.Søknaddokumentasjon
+import no.nav.familie.kontrakter.ba.søknad.v7.Søknadsvedlegg
+import no.nav.familie.kontrakter.ba.søknad.v8.AndreForelder
 import no.nav.familie.kontrakter.ba.søknad.v8.Arbeidsperiode
 import no.nav.familie.kontrakter.ba.søknad.v8.Barn
+import no.nav.familie.kontrakter.ba.søknad.v8.EøsBarnetrygdsperiode
+import no.nav.familie.kontrakter.ba.søknad.v8.Omsorgsperson
 import no.nav.familie.kontrakter.ba.søknad.v8.Pensjonsperiode
 import no.nav.familie.kontrakter.ba.søknad.v8.Utbetalingsperiode
 import no.nav.familie.kontrakter.felles.søknad.BaksSøknadBase
 import no.nav.familie.kontrakter.felles.søknad.BaksSøknadPersonBase
-import java.time.LocalDate
 import no.nav.familie.kontrakter.felles.søknad.Søknadsfelt as FellesSøknadsfelt
 
 data class BarnetrygdSøknad(
@@ -55,7 +59,40 @@ data class Søker(
     val idNummer: List<FellesSøknadsfelt<IdNummer>> = listOf(),
 ) : BaksSøknadPersonBase
 
+data class Barn(
+    override val ident: FellesSøknadsfelt<String>,
+    val harEøsSteg: Boolean,
+    val navn: FellesSøknadsfelt<String>,
+    val registrertBostedType: FellesSøknadsfelt<RegistrertBostedType>,
+    val alder: FellesSøknadsfelt<String>? = null,
+    val spørsmål: Map<String, FellesSøknadsfelt<Any>>,
+    val utenlandsperioder: List<FellesSøknadsfelt<Utenlandsopphold>> = listOf(),
+    val andreForelder: AndreForelder? = null,
+    val omsorgsperson: Omsorgsperson? = null,
+    val eøsBarnetrygdsperioder: List<FellesSøknadsfelt<EøsBarnetrygdsperiode>> = listOf(),
+    val idNummer: List<FellesSøknadsfelt<IdNummer>> = listOf(),
+    val svalbardOppholdPerioder: List<FellesSøknadsfelt<SvalbardPeriode>> = listOf(),
+) : BaksSøknadPersonBase
+
 data class SvalbardPeriode(
     val fraDatoSvalbardOpphold: Søknadsfelt<String?>,
     val tilDatoSvalbardOpphold: Søknadsfelt<String?>,
 )
+data class Søknaddokumentasjon(
+    val dokumentasjonsbehov: Dokumentasjonsbehov,
+    val harSendtInn: Boolean,
+    val opplastedeVedlegg: List<Søknadsvedlegg>,
+    val dokumentasjonSpråkTittel: Map<Locale, String>,
+)
+
+enum class Dokumentasjonsbehov {
+    AVTALE_DELT_BOSTED,
+    VEDTAK_OPPHOLDSTILLATELSE,
+    ADOPSJON_DATO,
+    BEKREFTELSE_FRA_BARNEVERN,
+    BOR_FAST_MED_SØKER,
+    SEPARERT_SKILT_ENKE,
+    MEKLINGSATTEST,
+    ANNEN_DOKUMENTASJON,
+    OPPHOLD_SVALBARD,
+}

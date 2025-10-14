bd7c11ae47790e4fa99e023c738dc57912051234
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2025-10-14 15:06:06 +0200

    legger til et eget felt for simuleringer fra alle fagsaker (#1078)

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/simulering/DetaljertSimuleringResultat.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/simulering/DetaljertSimuleringResultat.kt
@@ -2,4 +2,5 @@ package no.nav.familie.kontrakter.felles.simulering
 
 data class DetaljertSimuleringResultat(
     val simuleringMottaker: List<SimuleringMottaker>,
+    val simuleringMottakerAndreFagsaker: List<SimuleringMottaker> = emptyList(),
 )

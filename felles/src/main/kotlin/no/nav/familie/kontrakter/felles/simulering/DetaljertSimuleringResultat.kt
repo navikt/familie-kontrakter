55740c37e1c0e5bdeace937faa301a2612aa06a4
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2025-10-16 13:53:31 +0200

    fjern simuleringMottakerAndreFagsaker fra DetaljertSimuleringResultat.kt siden det ikke vil bli brukt allikevel

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/simulering/DetaljertSimuleringResultat.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/simulering/DetaljertSimuleringResultat.kt
@@ -2,5 +2,4 @@ package no.nav.familie.kontrakter.felles.simulering
 
 data class DetaljertSimuleringResultat(
     val simuleringMottaker: List<SimuleringMottaker>,
-    val simuleringMottakerAndreFagsaker: List<SimuleringMottaker> = emptyList(),
 )

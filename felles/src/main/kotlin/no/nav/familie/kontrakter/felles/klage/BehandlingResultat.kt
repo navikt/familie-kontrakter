dfd1fc5427b70b74740e5ba17a2498e33e0f0d39
endrehm <endre.heien.mo@nav.no>
2025-12-16 13:31:34 +0100

    Lagt til ny behandlingEvenType og to nye utfall

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/klage/BehandlingResultat.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/klage/BehandlingResultat.kt
@@ -33,6 +33,7 @@ enum class BehandlingEventType {
     BEHANDLING_FEILREGISTRERT,
     BEHANDLING_ETTER_TRYGDERETTEN_OPPHEVET_AVSLUTTET,
     OMGJOERINGSKRAVBEHANDLING_AVSLUTTET,
+    GJENOPPTAKSBEHANDLING_AVSLUTTET,
 }
 
 enum class KlageinstansUtfall(
@@ -51,4 +52,6 @@ enum class KlageinstansUtfall(
     INNSTILLING_AVVIST("Innstilling om avist til trygderetten fra KA"),
     HEVET("Hevet KA"),
     MEDHOLD_ETTER_FVL_35("Medhold etter fvl. § 35 KA"),
+    GJENOPPTATT_DELVIS_ELLER_FULLT_MEDHOLD("Gjenopptatt - Devlvis eller fullt medhold"),
+    GJENOPPTATT_OPPHEVET("Gjenopptatt - Opphevet"),
 }

2631d1441f9e07aeb31408800e4f3be76de933bc
Stig Helge Strøm <stig.helge.strom@nav.no>
2026-04-21 10:41:43 +0200

    Mer validering av barnetrygd-søknad
    Add recursive validerVerdi() helper for Any traversal
    Update validerFellesSøknadsfelt and validerSøknadsfelt to use recursive traversal
    Add teksterUtenomSpørsmål validation in valider()
    Add validerAndreForelder() method
    Add validerOmsorgsperson() method
    Wire andreForelder and omsorgsperson into validerBarn()

--- barnetrygd/src/main/kotlin/no/nav/familie/kontrakter/ba/søknad/v10/BarnetrygdSøknadV10Validator.kt
+++ barnetrygd/src/main/kotlin/no/nav/familie/kontrakter/ba/søknad/v10/BarnetrygdSøknadV10Validator.kt
@@ -5,6 +5,8 @@ import no.nav.familie.kontrakter.ba.søknad.UGYLDIGE_TEGN_REGEX
 import no.nav.familie.kontrakter.ba.søknad.Valideringsfeil
 import no.nav.familie.kontrakter.ba.søknad.v4.Locale
 import no.nav.familie.kontrakter.ba.søknad.v4.Søknadsfelt
+import no.nav.familie.kontrakter.ba.søknad.v8.AndreForelder
+import no.nav.familie.kontrakter.ba.søknad.v8.Omsorgsperson
 import no.nav.familie.kontrakter.felles.søknad.Søknadsfelt as FellesSøknadsfelt
 
 class BarnetrygdSøknadV10Validator {
@@ -23,6 +25,13 @@ class BarnetrygdSøknadV10Validator {
             // Valider spørsmål på toppnivå
             søknad.spørsmål.forEach { (spørsmålId, søknadsfelt) ->
                 feil.addAll(validerSøknadsfelt(søknadsfelt, "spørsmål.$spørsmålId"))
+            }
+
+            // Valider teksterUtenomSpørsmål
+            søknad.teksterUtenomSpørsmål.forEach { (spørsmålId, localeMap) ->
+                localeMap.forEach { (locale, tekst) ->
+                    feil.addAll(validerString(tekst, "teksterUtenomSpørsmål.$spørsmålId", locale))
+                }
             }
 
             return feil
@@ -125,6 +134,176 @@ class BarnetrygdSøknadV10Validator {
                 feil.addAll(validerFellesSøknadsfelt(periode, "$baseSti.svalbardOppholdPerioder[$index]"))
             }
 
+            // Valider andreForelder
+            barn.andreForelder?.let {
+                feil.addAll(validerAndreForelder(it, "$baseSti.andreForelder"))
+            }
+
+            // Valider omsorgsperson
+            barn.omsorgsperson?.let {
+                feil.addAll(validerOmsorgsperson(it, "$baseSti.omsorgsperson"))
+            }
+
+            return feil
+        }
+
+        private fun validerAndreForelder(
+            andreForelder: AndreForelder,
+            baseSti: String,
+        ): List<Valideringsfeil> {
+            val feil = mutableListOf<Valideringsfeil>()
+
+            // Enkeltfelt
+            feil.addAll(validerFellesSøknadsfelt(andreForelder.kanIkkeGiOpplysninger, "$baseSti.kanIkkeGiOpplysninger"))
+            andreForelder.navn?.let { feil.addAll(validerFellesSøknadsfelt(it, "$baseSti.navn")) }
+            andreForelder.fnr?.let { feil.addAll(validerFellesSøknadsfelt(it, "$baseSti.fnr")) }
+            andreForelder.fødselsdato?.let { feil.addAll(validerFellesSøknadsfelt(it, "$baseSti.fødselsdato")) }
+            andreForelder.arbeidUtlandet?.let { feil.addAll(validerFellesSøknadsfelt(it, "$baseSti.arbeidUtlandet")) }
+            andreForelder.pensjonUtland?.let { feil.addAll(validerFellesSøknadsfelt(it, "$baseSti.pensjonUtland")) }
+            andreForelder.skriftligAvtaleOmDeltBosted?.let { feil.addAll(validerFellesSøknadsfelt(it, "$baseSti.skriftligAvtaleOmDeltBosted")) }
+            andreForelder.pensjonNorge?.let { feil.addAll(validerFellesSøknadsfelt(it, "$baseSti.pensjonNorge")) }
+            andreForelder.arbeidNorge?.let { feil.addAll(validerFellesSøknadsfelt(it, "$baseSti.arbeidNorge")) }
+            andreForelder.andreUtbetalinger?.let { feil.addAll(validerFellesSøknadsfelt(it, "$baseSti.andreUtbetalinger")) }
+            andreForelder.adresse?.let { feil.addAll(validerFellesSøknadsfelt(it, "$baseSti.adresse")) }
+            andreForelder.pågåendeSøknadFraAnnetEøsLand?.let { feil.addAll(validerFellesSøknadsfelt(it, "$baseSti.pågåendeSøknadFraAnnetEøsLand")) }
+            andreForelder.pågåendeSøknadHvilketLand?.let { feil.addAll(validerFellesSøknadsfelt(it, "$baseSti.pågåendeSøknadHvilketLand")) }
+            andreForelder.barnetrygdFraEøs?.let { feil.addAll(validerFellesSøknadsfelt(it, "$baseSti.barnetrygdFraEøs")) }
+
+            // Lister
+            andreForelder.idNummer.forEachIndexed { index, felt ->
+                feil.addAll(validerFellesSøknadsfelt(felt, "$baseSti.idNummer[$index]"))
+            }
+            andreForelder.arbeidsperioderUtland.forEachIndexed { index, felt ->
+                feil.addAll(validerFellesSøknadsfelt(felt, "$baseSti.arbeidsperioderUtland[$index]"))
+            }
+            andreForelder.pensjonsperioderUtland.forEachIndexed { index, felt ->
+                feil.addAll(validerFellesSøknadsfelt(felt, "$baseSti.pensjonsperioderUtland[$index]"))
+            }
+            andreForelder.arbeidsperioderNorge.forEachIndexed { index, felt ->
+                feil.addAll(validerFellesSøknadsfelt(felt, "$baseSti.arbeidsperioderNorge[$index]"))
+            }
+            andreForelder.pensjonsperioderNorge.forEachIndexed { index, felt ->
+                feil.addAll(validerFellesSøknadsfelt(felt, "$baseSti.pensjonsperioderNorge[$index]"))
+            }
+            andreForelder.andreUtbetalingsperioder.forEachIndexed { index, felt ->
+                feil.addAll(validerFellesSøknadsfelt(felt, "$baseSti.andreUtbetalingsperioder[$index]"))
+            }
+            andreForelder.eøsBarnetrygdsperioder.forEachIndexed { index, felt ->
+                feil.addAll(validerFellesSøknadsfelt(felt, "$baseSti.eøsBarnetrygdsperioder[$index]"))
+            }
+
+            // Utvidet
+            andreForelder.utvidet.søkerHarBoddMedAndreForelder?.let {
+                feil.addAll(validerFellesSøknadsfelt(it, "$baseSti.utvidet.søkerHarBoddMedAndreForelder"))
+            }
+            andreForelder.utvidet.søkerFlyttetFraAndreForelderDato?.let {
+                feil.addAll(validerFellesSøknadsfelt(it, "$baseSti.utvidet.søkerFlyttetFraAndreForelderDato"))
+            }
+
+            return feil
+        }
+
+        private fun validerOmsorgsperson(
+            omsorgsperson: Omsorgsperson,
+            baseSti: String,
+        ): List<Valideringsfeil> {
+            val feil = mutableListOf<Valideringsfeil>()
+
+            // Enkeltfelt
+            feil.addAll(validerFellesSøknadsfelt(omsorgsperson.navn, "$baseSti.navn"))
+            feil.addAll(validerFellesSøknadsfelt(omsorgsperson.slektsforhold, "$baseSti.slektsforhold"))
+            omsorgsperson.slektsforholdSpesifisering?.let { feil.addAll(validerFellesSøknadsfelt(it, "$baseSti.slektsforholdSpesifisering")) }
+            feil.addAll(validerFellesSøknadsfelt(omsorgsperson.idNummer, "$baseSti.idNummer"))
+            feil.addAll(validerFellesSøknadsfelt(omsorgsperson.adresse, "$baseSti.adresse"))
+            feil.addAll(validerFellesSøknadsfelt(omsorgsperson.arbeidUtland, "$baseSti.arbeidUtland"))
+            feil.addAll(validerFellesSøknadsfelt(omsorgsperson.arbeidNorge, "$baseSti.arbeidNorge"))
+            feil.addAll(validerFellesSøknadsfelt(omsorgsperson.pensjonUtland, "$baseSti.pensjonUtland"))
+            feil.addAll(validerFellesSøknadsfelt(omsorgsperson.pensjonNorge, "$baseSti.pensjonNorge"))
+            feil.addAll(validerFellesSøknadsfelt(omsorgsperson.andreUtbetalinger, "$baseSti.andreUtbetalinger"))
+            feil.addAll(validerFellesSøknadsfelt(omsorgsperson.pågåendeSøknadFraAnnetEøsLand, "$baseSti.pågåendeSøknadFraAnnetEøsLand"))
+            omsorgsperson.pågåendeSøknadHvilketLand?.let { feil.addAll(validerFellesSøknadsfelt(it, "$baseSti.pågåendeSøknadHvilketLand")) }
+            feil.addAll(validerFellesSøknadsfelt(omsorgsperson.barnetrygdFraEøs, "$baseSti.barnetrygdFraEøs"))
+
+            // Lister
+            omsorgsperson.arbeidsperioderUtland.forEachIndexed { index, felt ->
+                feil.addAll(validerFellesSøknadsfelt(felt, "$baseSti.arbeidsperioderUtland[$index]"))
+            }
+            omsorgsperson.arbeidsperioderNorge.forEachIndexed { index, felt ->
+                feil.addAll(validerFellesSøknadsfelt(felt, "$baseSti.arbeidsperioderNorge[$index]"))
+            }
+            omsorgsperson.pensjonsperioderUtland.forEachIndexed { index, felt ->
+                feil.addAll(validerFellesSøknadsfelt(felt, "$baseSti.pensjonsperioderUtland[$index]"))
+            }
+            omsorgsperson.pensjonsperioderNorge.forEachIndexed { index, felt ->
+                feil.addAll(validerFellesSøknadsfelt(felt, "$baseSti.pensjonsperioderNorge[$index]"))
+            }
+            omsorgsperson.andreUtbetalingsperioder.forEachIndexed { index, felt ->
+                feil.addAll(validerFellesSøknadsfelt(felt, "$baseSti.andreUtbetalingsperioder[$index]"))
+            }
+            omsorgsperson.eøsBarnetrygdsperioder.forEachIndexed { index, felt ->
+                feil.addAll(validerFellesSøknadsfelt(felt, "$baseSti.eøsBarnetrygdsperioder[$index]"))
+            }
+
+            return feil
+        }
+
+        private fun validerString(
+            verdi: String,
+            objectPath: String,
+            locale: Locale,
+        ): List<Valideringsfeil> {
+            val feil = mutableListOf<Valideringsfeil>()
+
+            if (verdi.length > MAKS_LENGDE) {
+                feil.add(
+                    Valideringsfeil(
+                        objectPath = objectPath,
+                        locale = locale,
+                        feilmelding = "Verdi overskrider maksimal lengde på $MAKS_LENGDE tegn (faktisk: ${verdi.length})",
+                    ),
+                )
+            }
+
+            if (UGYLDIGE_TEGN_REGEX.containsMatchIn(verdi)) {
+                feil.add(
+                    Valideringsfeil(
+                        objectPath = objectPath,
+                        locale = locale,
+                        feilmelding = "Verdi inneholder ugyldige tegn: $verdi",
+                    ),
+                )
+            }
+
+            return feil
+        }
+
+        private fun validerVerdiRekursivt(
+            verdi: Any?,
+            objectPath: String,
+            locale: Locale,
+        ): List<Valideringsfeil> {
+            if (verdi == null) return emptyList()
+
+            val feil = mutableListOf<Valideringsfeil>()
+
+            when (verdi) {
+                is String -> {
+                    feil.addAll(validerString(verdi, objectPath, locale))
+                }
+
+                is Map<*, *> -> {
+                    verdi.forEach { (key, value) ->
+                        feil.addAll(validerVerdiRekursivt(value, "$objectPath.$key", locale))
+                    }
+                }
+
+                is Collection<*> -> {
+                    verdi.forEachIndexed { index, element ->
+                        feil.addAll(validerVerdiRekursivt(element, "$objectPath[$index]", locale))
+                    }
+                }
+            }
+
             return feil
         }
 
@@ -156,27 +335,9 @@ class BarnetrygdSøknadV10Validator {
                 }
             }
 
-            // Valider verdi - sjekk kun String-verdier
+            // Valider verdi - traverser rekursivt for å finne alle String-verdier
             søknadsfelt.verdi.forEach { (locale, verdi) ->
-                if (verdi is String && verdi.length > MAKS_LENGDE) {
-                    feil.add(
-                        Valideringsfeil(
-                            objectPath = "$objectPath.verdi",
-                            locale = locale,
-                            feilmelding = "Verdi overskrider maksimal lengde på $MAKS_LENGDE tegn (faktisk: ${verdi.length})",
-                        ),
-                    )
-                }
-
-                if (verdi is String && UGYLDIGE_TEGN_REGEX.containsMatchIn(verdi)) {
-                    feil.add(
-                        Valideringsfeil(
-                            objectPath = "$objectPath.verdi",
-                            locale = locale,
-                            feilmelding = "Verdi inneholder ugyldige tegn: $verdi",
-                        ),
-                    )
-                }
+                feil.addAll(validerVerdiRekursivt(verdi, "$objectPath.verdi", locale))
             }
 
             return feil
@@ -210,27 +371,9 @@ class BarnetrygdSøknadV10Validator {
                 }
             }
 
-            // Valider verdi - sjekk kun String-verdier
+            // Valider verdi - traverser rekursivt for å finne alle String-verdier
             søknadsfelt.verdi.forEach { (locale, verdi) ->
-                if (verdi is String && verdi.length > MAKS_LENGDE) {
-                    feil.add(
-                        Valideringsfeil(
-                            objectPath = "$objectPath.verdi",
-                            locale = locale,
-                            feilmelding = "Verdi overskrider maksimal lengde på $MAKS_LENGDE tegn (faktisk: ${verdi.length})",
-                        ),
-                    )
-                }
-
-                if (verdi is String && UGYLDIGE_TEGN_REGEX.containsMatchIn(verdi)) {
-                    feil.add(
-                        Valideringsfeil(
-                            objectPath = "$objectPath.verdi",
-                            locale = locale,
-                            feilmelding = "Verdi inneholder ugyldige tegn: $verdi",
-                        ),
-                    )
-                }
+                feil.addAll(validerVerdiRekursivt(verdi, "$objectPath.verdi", locale))
             }
 
             return feil

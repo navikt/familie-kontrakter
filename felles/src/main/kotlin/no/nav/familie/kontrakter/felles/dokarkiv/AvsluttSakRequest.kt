aac934f70a26e21fa78481c730878db281c1dd10
Uy Nguyen <uy.quang.nguyen@nav.no>
2026-05-05 11:36:05 +0200

    Juster på felles avsluttsakrequest

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/dokarkiv/AvsluttSakRequest.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/dokarkiv/AvsluttSakRequest.kt
@@ -1,13 +1,15 @@
 package no.nav.familie.kontrakter.felles.dokarkiv
 
 import com.fasterxml.jackson.annotation.JsonInclude
+import no.nav.familie.kontrakter.felles.Fagsystem
+import no.nav.familie.kontrakter.felles.Tema
 import java.time.LocalDateTime
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class AvsluttSakRequest(
-    val tema: String,
+    val tema: Tema,
     val fagsakId: String,
-    val fagsaksystem: String,
+    val fagsaksystem: Fagsystem,
     val bruker: DokarkivBruker,
     val opprettetDato: LocalDateTime,
     val administrativEnhet: String,

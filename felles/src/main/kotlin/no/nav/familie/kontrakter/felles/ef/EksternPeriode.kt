10745356c00f5c6e6251d1b94a887d129a1acebf
RobinLundSadun <robin.lund.sadun@gmail.com>
2026-07-29 14:49:10 +0200

    Legger til boolean på classe som sendes til ts

--- felles/src/main/kotlin/no/nav/familie/kontrakter/felles/ef/EksternPeriode.kt
+++ felles/src/main/kotlin/no/nav/familie/kontrakter/felles/ef/EksternPeriode.kt
@@ -56,4 +56,5 @@ data class EksternPeriodeMedStønadstype(
     val fomDato: LocalDate,
     val tomDato: LocalDate,
     val stønadstype: StønadType,
+    val erRegelendring2026: Boolean = false,
 )
